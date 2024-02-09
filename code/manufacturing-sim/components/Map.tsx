b6c49d57eaf803121fc42d68f229a159e16f1082
tachorzy <tachorzyad@gmail.com>
2024-02-09 01:20:13 -0600

    added World.json

--- code/manufacturing-sim/components/Map.tsx
+++ code/manufacturing-sim/components/Map.tsx
@@ -4,7 +4,7 @@ import React from "react"
 import { ComposableMap, Geographies, Geography } from "react-simple-maps"
 
 const geoUrl =
-  "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json"
+  ""
 
 const Map = () => {
 
