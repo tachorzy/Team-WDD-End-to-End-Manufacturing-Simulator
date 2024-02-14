aa19ad303fde21cb116c6b08edb8a013c3562c81
ReginaldGreen02 <rejogreen@gmail.com>
2024-02-14 13:45:51 -0600

    Applied DRY to Dashboard to add more tiles

--- code/manufacturing-sim/components/Dashboard.tsx
+++ code/manufacturing-sim/components/Dashboard.tsx
@@ -2,11 +2,17 @@ import React from 'react'
 import Tile  from "./Tiles"
 
 const Dashboard: React.FC = () => {
+    const tileData =[
+        { label: 'Sites', value: 10 },
+        { label: 'Gateways', value: 5 },
+        { label: 'Assets', value: 100 },
+    ]
+    
     return (
       <div>
-        <Tile value={10} label="Sites" />
-        <Tile value={5} label="Gateways" />
-        <Tile value={100} label="Assets" />
+        {tileData.map((tile,index) => (
+            <Tile key={index} value={tile.value} label={tile.label} />
+        ))}
       </div>
     );
   };
