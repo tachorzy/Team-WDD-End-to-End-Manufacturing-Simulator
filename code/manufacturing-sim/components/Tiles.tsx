fda7815adf4f0905ea181c3c679e6c48e53e2925
ReginaldGreen02 <rejogreen@gmail.com>
2024-02-14 13:52:26 -0600

    Styling: Increased Size of Tile when hovered over

--- code/manufacturing-sim/components/Tiles.tsx
+++ code/manufacturing-sim/components/Tiles.tsx
@@ -7,7 +7,7 @@ interface TileProps {
 
 const Tile: React.FC<TileProps> = ({ label, value }) => {
   return (
-    <div className="p-4 m-4 text-center inline-block float-left w-52 bg-blue-500">
+    <div className="p-4 m-4 text-center inline-block float-left w-52 bg-blue-500 rounded-xl transition-transform transform hover:scale-110 ">
       <div>{label}</div>
       <div className="text-2xl font-bold">{value}</div>
     </div>
