e32b4f2e81575b316cadb47b1af8030970d096a1
Cairo A <cairohyfr@gmail.com>
2024-02-15 10:07:57 -0600

    fix: zooming into newly added factory location on map

--- code/manufacturing-sim/components/home/Map.client.tsx
+++ code/manufacturing-sim/components/home/Map.client.tsx
@@ -1,6 +1,6 @@
 'use client';
 // import font later
-import React from "react";
+import React,{useEffect} from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
@@ -16,11 +16,19 @@ const customIcon = new L.Icon({
   popupAnchor: [0, -35]
 });
 
-const ChangeView = ({ center, zoom }) => {
+const ChangeView =({center,zoom}) =>{
   const map = useMap();
-  map.flyTo(center, zoom);
+
+  useEffect(()=>{
+    if (center){
+      map.flyTo(center,zoom,{
+        animate:true,
+        duration:2.0
+      });
+    }
+  },[center,zoom,map]);
   return null;
-};
+}
 
 const MapComponent: React.FC<MapProps> = ({ positions })=> {
   const initialZoom = 4;
@@ -30,14 +38,16 @@ const MapComponent: React.FC<MapProps> = ({ positions })=> {
     <div>
       <MapContainer center={[37.0902, -95.7129]} zoom={initialZoom} style={{ height: '25rem', width: '100%' }}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {positions.length > 0 && (
+          <ChangeView center={positions[positions.length - 1]} zoom={zoomInLevel} />
+        )}
         {positions.map((position, index) => (
           <Marker key={index} position={[position.lat, position.lon]} icon={customIcon}>
-            <Popup>Factory Data or we can redirect user to another page whnen they click on it</Popup>
+            <Popup>Factory Data or we can redirect user to another page when they click on it</Popup>
           </Marker>
         ))}
       </MapContainer>
     </div>
-
   );
 };
 
