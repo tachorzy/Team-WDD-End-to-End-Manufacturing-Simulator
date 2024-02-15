87ee8dbb991a49c354305e2c8cce4ebff5edd1be
ReginaldGreen02 <rejogreen@gmail.com>
2024-02-15 15:48:08 -0600

    feat: modified files to allow  eslint to not throw errors
    Postcss.json and coverage were causing errors since they were not being found in tsconfig while . I modified tsconfig.json to include  postcss.json and created a .eslintignore in order to not go over coverage. This is because we should not be modifying coverage as it will be generated at runtime.

--- code/manufacturing-sim/postcss.config.js
+++ code/manufacturing-sim/postcss.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname 
- }
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+    parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: __dirname,
+    },
 };
