15753e67b08cdc029c64e81239271a2577bd2541
ReginaldGreen02 <rejogreen@gmail.com>
2024-02-14 23:33:21 -0600

    fix: parsing error fixed by adding tsconfigRootDir
    https://stackoverflow.com/questions/65137939/typescript-eslint-throws-cannot-read-file-tsconfig-json-in-every-project-i-s

--- code/manufacturing-sim/postcss.config.js
+++ code/manufacturing-sim/postcss.config.js
@@ -3,4 +3,8 @@ module.exports = {
     tailwindcss: {},
     autoprefixer: {},
   },
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname // <-- this did the trick for me
+ }
 };
