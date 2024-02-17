c551f7c024260c73273fc602c69a357a6ada68f2
tachorzy <tachorzyad@gmail.com>
2024-02-17 13:31:07 -0600

    style: added smooth scrolling to globals.css (wasn't working elsewhere)

--- code/manufacturing-sim/app/page.tsx
+++ code/manufacturing-sim/app/page.tsx
@@ -7,6 +7,7 @@ import LandingPageTitle from '@/components/home/LandingPageTitle';
 import Map from "../components/home/Map.client";
 import Searchbar from "../components/home/Searchbar.client";
 import Navbar from "../components/navbar/Navbar";
+import Link from "next/link";
 
 export default function Home() {
   const [positions, setPositions] = useState<Array<{ lat: number, lon: number }>>([]);
@@ -15,19 +16,19 @@ export default function Home() {
   };
   
   return(
-    <main className="flex flex-col bg-[#FAFAFA] min-h-screen mx-auto">
+    <main className="flex flex-col bg-[#FAFAFA] min-h-screen mx-auto smooth-scroll">
       <div className="flex flex-col bg-[url('/background/Grid.svg')] max-h-1/2 rounded-3xl bg-opacity-[15%]">
         <div className="px-32">
           <Navbar/>
           <div className="flex flex-col block items-center justify-center gap-y-16 mt-16 mx-auto">
             <LandingPageTitle/>
-            <button className="rounded-full bg-gradient-to-br from-DarkGray via-[#555F68] to-DarkGray opacity-[95%] border-solid border-2 border-neutral-400 p-3 transform transition duration-500 hover:scale-[102.5%] hover:border-MainBlue font-semibold">
+            <Link href="#searchbar" className="rounded-full bg-gradient-to-br from-DarkGray via-[#555F68] to-DarkGray opacity-[95%] border-solid border-2 border-neutral-400 p-3 transform transition duration-500 hover:scale-[102.5%] hover:border-MainBlue font-semibold">
               Define your industry
-            </button> 
+            </Link> 
           </div>
 
           <div className="flex flex-col items-center justify-center mt-[50%] gap-y-8">
-            <div>
+            <div id="searchbar">
               <Searchbar onSearch={handleNewLocation}/>
             </div>
             <div className="w-full max-w-4xl shadow-lg rounded-lg mb-4">
