9613c03b024b7975f4e4e2c0c02d574c4f095c47
tachorzy <tachorzyad@gmail.com>
2024-02-17 00:43:01 -0600

    style: added animation to searchmode button

--- code/manufacturing-sim/components/home/Searchbar.client.tsx
+++ code/manufacturing-sim/components/home/Searchbar.client.tsx
@@ -68,8 +68,8 @@ const Searchbar: React.FC<SearchProps> = ({ onSearch }) => {
         </div>
       }
 
-      <button onClick={() => setIsAddressSearchBarActive(!isAddressSearchBarActive)} className="flex flex-col bg-MainBlue dark:text-white rounded p-2.5 w-20 font-bold transition-colors duration-700 ease-in ease-out hover:scale-[101%] items-center">
-        <Image src="/icons/searchbar/cycle.svg" width={35} height={35} alt="switch searchbar"></Image>
+      <button onClick={() => setIsAddressSearchBarActive(!isAddressSearchBarActive)} className="flex flex-col group bg-MainBlue dark:text-white rounded p-2.5 w-20 font-bold transition-colors duration-700 ease-in ease-out hover:scale-[101%] items-center">
+        <Image src="/icons/searchbar/cycle.svg" width={35} height={35} alt="switch searchbar" className="group-hover:rotate-180 transform duration-500"></Image>
 
       </button>
 
