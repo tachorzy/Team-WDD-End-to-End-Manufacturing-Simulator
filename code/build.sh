1aac33a9b314d4c4e9564ede1b25b60dd1a6de75
Bryant Le <bryantle2021@gmail.com>
2024-02-18 19:49:01 -0600

    ci: update cd commands

--- code/build.sh
+++ code/build.sh
@@ -1,12 +1,11 @@
 # backend
-# cd code/backend/api
-# go get ./...
-# go build -o build/main -v cmd/main.go
-# go test ./...
+cd ./backend/api
+go get ./...
+go build -o build/main -v cmd/main.go
+go test ./...
 
-# frontend|
-# npm install
-# npm ci
-# cd code/manufacturing-sim
-# npm run lint:fix
-# npm run test:coverage
+# frontend
+cd ../../manufacturing-sim
+npm ci
+npm run lint:fix
+npm run test:coverage
