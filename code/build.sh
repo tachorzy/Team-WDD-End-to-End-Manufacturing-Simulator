918bc5effd74ac9eee0bce9a03151851cc0a4970
Bryant Le <bryantle2021@gmail.com>
2024-02-18 20:55:15 -0600

    ci: add backend installation commands

--- code/build.sh
+++ code/build.sh
@@ -1,51 +1,90 @@
-# Install Node.js and npm
-NODE_VERSION="20.0.0"
-PLATFORM="linux-x64"
+#!/bin/bash
 
+# Go installation variables
+GO_VERSION="1.20"
+GO_PLATFORM="linux-amd64"
+GO_DIR="$HOME/go"
+GO_BIN="$HOME/go/bin"
+
+# Node.js installation variables
+NODE_VERSION="20.0.0"
+NODE_PLATFORM="linux-x64"
 NODE_DIR="$HOME/node"
 
-check_node_exist() {
-    if [ -x "$NODE_DIR/bin/node" ]; then
-        INSTALLED_VERSION=$($NODE_DIR/bin/node -v)
-        if [ "v$NODE_VERSION" = "$INSTALLED_VERSION" ]; then
+check_go_exist() {
+    if [ -x "$GO_BIN/go" ]; then
+        INSTALLED_GO_VERSION=$($GO_BIN/go version | awk '{print $3}')
+        if [ "go$GO_VERSION" = "$INSTALLED_GO_VERSION" ]; then
             return 0
         fi
     fi
     return 1
 }
 
+install_go() {
+  # Create Node.js directory
+  mkdir -p "$GO_DIR"
+  # Download Go tarball
+  curl -o "$GO_DIR/go$GO_VERSION.$GO_PLATFORM.tar.gz" "https://golang.org/dl/go$GO_VERSION.$GO_PLATFORM.tar.gz"
+  # Extract Go tarball
+  tar -C "$HOME" -xzf "$GO_DIR/go$GO_VERSION.$GO_PLATFORM.tar.gz"
+  # Clean up the tarball
+  rm "$GO_DIR/go$GO_VERSION.$GO_PLATFORM.tar.gz"
+}
+
+# Check and install Go if necessary
+if check_go_exist; then
+  echo "Go v$GO_VERSION is already installed."
+else
+  echo "Go v$GO_VERSION is not installed. Installing now..."
+  install_go
+
+  # Verify installation
+  go version
+fi
+
+check_node_exist() {
+  if [ -x "$NODE_DIR/bin/node" ]; then
+    INSTALLED_VERSION=$($NODE_DIR/bin/node -v)
+    if [ "v$NODE_VERSION" = "$INSTALLED_VERSION" ]; then
+      return 0
+    fi
+  fi
+  return 1
+}
+
 install_node() {
   # Create Node.js directory
   mkdir -p "$NODE_DIR"
-  # Download Node.js binary
+  # Download Node.js tarball
   curl -o "$NODE_DIR/node-$NODE_VERSION-$PLATFORM.tar.xz" "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-$PLATFORM.tar.xz"
-  # Extract Node.js
+  # Extract Node.js tarball
   tar -xJf "$NODE_DIR/node-$NODE_VERSION-$PLATFORM.tar.xz" -C "$NODE_DIR" --strip-components=1
   # Clean up the tarball
   rm "$NODE_DIR/node-$NODE_VERSION-$PLATFORM.tar.xz"
 }
 
+# Check and install Node.js if necessary
 if check_node_exist; then
-    echo "Node.js v$NODE_VERSION is already installed."
+  echo "Node.js v$NODE_VERSION is already installed."
 else
-    echo "Node.js v$NODE_VERSION is not installed. Installing now..."
-    install_node
+  echo "Node.js v$NODE_VERSION is not installed. Installing now..."
+  install_node
 
-    # Verify installion
-    node -v
-    npm -v
+  # Verify installion
+  node -v
+  npm -v
 fi
 
-# Update PATH to include the Node.js binary
-export PATH="$NODE_DIR/bin:$PATH"
+# Update PATH to include the Go and Node.js binary
+export PATH="$GO_BIN:$NODE_DIR/bin:$PATH"
 
-# backend
-# cd ./backend/api
-# go get ./...
-# go build -o build/main -v cmd/main.go
-# go test ./...
+# Run backend script
+cd backend/api
+chmod +x build.sh
+./build.sh
 
-# frontend
-cd manufacturing-sim
+# Run frontend script
+cd ../../manufacturing-sim
 chmod +x build.sh
 ./build.sh
