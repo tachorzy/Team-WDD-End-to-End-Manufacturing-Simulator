8ec280ee0afd2486b96322749008758a90b62bb3
Bryant Le <bryantle2021@gmail.com>
2024-02-18 19:54:11 -0600

    ci: comment backend commands

--- code/build.sh
+++ code/build.sh
@@ -1,11 +1,11 @@
 # backend
-cd ./backend/api
-go get ./...
-go build -o build/main -v cmd/main.go
-go test ./...
+# cd ./backend/api
+# go get ./...
+# go build -o build/main -v cmd/main.go
+# go test ./...
 
 # frontend
-cd ../../manufacturing-sim
+cd ./manufacturing-sim
 npm ci
 npm run lint:fix
 npm run test:coverage
