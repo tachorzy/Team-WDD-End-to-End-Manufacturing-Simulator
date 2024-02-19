dd241980092b4bf2a664ec57a95eebe008c72d99
ReginaldGreen02 <115752371+ReginaldGreen02@users.noreply.github.com>
2024-02-18 19:09:33 -0600

    fix: removed commands from build.sh
    Going to ask Venkat for guidance

--- code/build.sh
+++ code/build.sh
@@ -1,12 +1,12 @@
 # backend
-cd code/backend/api
-go get ./...
-go build -o build/main -v cmd/main.go
-go test ./...
+# cd code/backend/api
+# go get ./...
+# go build -o build/main -v cmd/main.go
+# go test ./...
 
 # frontend|
-npm install
-npm ci
-cd code/manufacturing-sim
-npm run lint:fix
-npm run test:coverage
+# npm install
+# npm ci
+# cd code/manufacturing-sim
+# npm run lint:fix
+# npm run test:coverage
