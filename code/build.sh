cadefc9d400f06736474430b75233c52d3f9b7e1
Bryant Le <bryantle2021@gmail.com>
2024-02-18 20:44:19 -0600

    ci: add node exists case

--- code/build.sh
+++ code/build.sh
@@ -3,29 +3,40 @@ NODE_VERSION="20.0.0"
 PLATFORM="linux-x64"
 
 NODE_DIR="$HOME/node"
-mkdir -p "$NODE_DIR"
 
-# Download Node.js binary
-echo "Downloading Node.js..."
-curl -o "$NODE_DIR/node-$NODE_VERSION-$PLATFORM.tar.xz" "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-$PLATFORM.tar.xz"
+check_node_exist() {
+    if [ -x "$NODE_DIR/bin/node" ]; then
+        INSTALLED_VERSION=$($NODE_DIR/bin/node -v)
+        if [ "v$NODE_VERSION" = "$INSTALLED_VERSION" ]; then
+            return 0
+        fi
+    fi
+    return 1
+}
 
-# Extract Node.js
-echo "Extracting Node.js..."
-tar -xJf "$NODE_DIR/node-$NODE_VERSION-$PLATFORM.tar.xz" -C "$NODE_DIR" --strip-components=1
+install_node() {
+  # Create Node.js directory
+  mkdir -p "$NODE_DIR"
+  # Download Node.js binary
+  curl -o "$NODE_DIR/node-$NODE_VERSION-$PLATFORM.tar.xz" "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-$PLATFORM.tar.xz"
+  # Extract Node.js
+  tar -xJf "$NODE_DIR/node-$NODE_VERSION-$PLATFORM.tar.xz" -C "$NODE_DIR" --strip-components=1
+  # Clean up the tarball
+  rm "$NODE_DIR/node-$NODE_VERSION-$PLATFORM.tar.xz"
+}
 
-# Clean up the tarball
-rm "$NODE_DIR/node-$NODE_VERSION-$PLATFORM.tar.xz"
+if check_node_exist; then
+    echo "Node.js v$NODE_VERSION is already installed."
+else
+    echo "Node.js v$NODE_VERSION is not installed. Installing now..."
+    install_node
+
+    # Verify installion
+    node -v
+    npm -v
 
 # Update PATH to include the Node.js binary
 export PATH="$NODE_DIR/bin:$PATH"
-
-# Verify installation
-echo "Node.js and npm versions:"
-node -v
-npm -v
-
-
-
 
 # backend
 # cd ./backend/api
