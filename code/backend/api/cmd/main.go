f4b714f90d0357ced59ffcb380e6c307184c5a91
C <106287207+citruscai@users.noreply.github.com>
2024-02-23 13:41:12 -0600

    feature: create read factory endpoint (#7)
    * feature: create read factory endpoint
    
    * fix: missing "package main" line in read factory boilerpalate
    
    * fix: backend merge conflicts
    
    * chore: reformat go files
    
    * fix: merge backend conflicts
    
    * feature: create read factory endpoint
    
    * fix: fixing syntax errors
    
    * fix: read factory error and returning in json format for frontend
    
    ---------
    
    Co-authored-by: Bryant Le <bryantle2021@gmail.com>
    Co-authored-by: Bryant Le <80173797+bnleft@users.noreply.github.com>

--- code/backend/api/cmd/main.go
+++ code/backend/api/cmd/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-    "github.com/aws/aws-lambda-go/lambda"
-    "wdd/api/pkg/routes"
+	"github.com/aws/aws-lambda-go/lambda"
+	"wdd/api/pkg/routes"
 )
 
 func main() {
-    handler := routes.InitializeRoutes()
-    lambda.Start(handler)
+	handler := routes.InitializeRoutes()
+	lambda.Start(handler)
 }
