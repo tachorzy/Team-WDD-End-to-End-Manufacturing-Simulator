000efd629b46d82f6b00fe5830a8fab4c235837d
tachorzy <tachorzyad@gmail.com>
2024-02-23 03:50:04 -0600

    chore: installed inter-tight fonts, andadded localNextFont.ts (not working yet)

--- code/manufacturing-sim/app/layout.tsx
+++ code/manufacturing-sim/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { interTightRegular } from "../utils/localNextFonts";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${inter.className} scroll-smooth`}>
+            <body className={`${interTightRegular.className} scroll-smooth`}>
                 {children}
             </body>
         </html>
