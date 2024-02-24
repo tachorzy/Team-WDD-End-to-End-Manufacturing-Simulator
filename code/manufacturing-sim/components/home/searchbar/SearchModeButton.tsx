00c55e8a388adeaf2610c27cfef4a45508f3162d
tachorzy <tachorzyad@gmail.com>
2024-02-23 19:26:37 -0600

    style: adjusted SearchModeButton text

--- code/manufacturing-sim/components/home/searchbar/SearchModeButton.tsx
+++ code/manufacturing-sim/components/home/searchbar/SearchModeButton.tsx
@@ -4,22 +4,23 @@ import Link from "next/link";
 
 const SearchModeButton = (props: {isAddressSearchBarActive: boolean, setIsAddressSearchBarActive: React.Dispatch<React.SetStateAction<boolean>>, setInvalidInput: React.Dispatch<React.SetStateAction<boolean>>}) => {
     return (
-        <div className="flex flex-row rounded w-64">
+        <div className="flex flex-row rounded w-80">
             <button
                 type="button"
                 onClick={() => {
                     props.setIsAddressSearchBarActive(!props.isAddressSearchBarActive);
                     props.setInvalidInput(false);
                 }}
-                className={(props.isAddressSearchBarActive ? "bg-MainBlue text-white" : "bg-LightGray text-MainBlue") + " flex flex-col group rounded-l border-[3px] border-MainBlue p-2.5 h-10 w-1/2 font-bold transition-colors duration-700 ease-in ease-out hover:scale-[101%] items-center"}
+                className={(props.isAddressSearchBarActive ? "bg-MainBlue text-white" : "bg-LightGray text-MainBlue") + " flex flex-row gap-x-1 group rounded-l border-[3px] border-MainBlue p-2.5 h-10 w-1/2 text-sm font-semibold transition-colors duration-700 ease-in ease-out hover:scale-[101%] items-center justify-center"}
             >
                 <Image
                     src="/icons/searchbar/address.svg"
-                    width={20}
-                    height={20}
+                    width={25}
+                    height={25}
                     alt="switch searchbar"
                     className="group-hover:rotate-180 transform duration-500"
                 />
+                {"Address"}
             </button>
             <button
                 type="button"
@@ -27,15 +28,16 @@ const SearchModeButton = (props: {isAddressSearchBarActive: boolean, setIsAddres
                     props.setIsAddressSearchBarActive(!props.isAddressSearchBarActive);
                     props.setInvalidInput(false);
                 }}
-                className={(!props.isAddressSearchBarActive ? "bg-MainBlue text-white" : "bg-LightGray text-MainBlue") + " flex flex-col group dark:text-white rounded-r border-[3px] border-MainBlue p-2.5 h-10 w-1/2 font-bold transition-colors duration-700 ease-in ease-out hover:scale-[101%] items-center"}
+                className={(!props.isAddressSearchBarActive ? "bg-MainBlue text-white" : "bg-LightGray text-MainBlue") + " flex flex-row gap-x-1 group rounded-r border-[3px] border-MainBlue p-2.5 h-10 w-1/2 text-sm font-semibold transition-colors duration-700 ease-in ease-out hover:scale-[101%] items-center justify-center"}
             >
                 <Image
-                    src="/icons/searchbar/address.svg"
-                    width={20}
-                    height={20}
+                    src="/icons/searchbar/compass.svg"
+                    width={25}
+                    height={25}
                     alt="switch searchbar"
                     className="group-hover:rotate-180 transform duration-500"
                 />
+                {"Coordinates"}
             </button>
         </div>
     );
