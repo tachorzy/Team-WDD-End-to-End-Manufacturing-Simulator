752c42e4e0f3b4256b07178ee7820e1b9362cc84
tachorzy <tachorzyad@gmail.com>
2024-02-24 16:09:09 -0600

    style: added table results (static data)

--- code/manufacturing-sim/components/home/FactoryTable.client.tsx
+++ code/manufacturing-sim/components/home/FactoryTable.client.tsx
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-const FactoryTable = (props: { }) => {
+// interface Facility {
+//     name: string;
+//     address: string;
+//     lat: number;
+//     lon: number;
+//     city: string;
+//     state: string;
+//     country: string;
+//     OEEE: number;
+//     lastUpdate: string;
+// }
+
+const FactoryTable = () => {
+
+    const STATIC_DATA_REPLACE_LATER = [
+        {name: "Facility 1", address: "1234 Main St", lat: 123.456, lon: 123.456, city: "City", state: "State", country: "Country", OEEE: 0.85, lastUpdate: "2022-01-01"},
+        {name: "Facility 1", address: "1234 Main St", lat: 123.456, lon: 123.456, city: "City", state: "State", country: "Country", OEEE: 0.85, lastUpdate: "2022-01-01"},
+        {name: "Facility 1", address: "1234 Main St", lat: 123.456, lon: 123.456, city: "City", state: "State", country: "Country", OEEE: 0.85, lastUpdate: "2022-01-01"},
+        {name: "Facility 1", address: "1234 Main St", lat: 123.456, lon: 123.456, city: "City", state: "State", country: "Country", OEEE: 0.85, lastUpdate: "2022-01-01"},
+        {name: "Facility 1", address: "1234 Main St", lat: 123.456, lon: 123.456, city: "City", state: "State", country: "Country", OEEE: 0.85, lastUpdate: "2022-01-01"}
+    ] 
+
+    const [facilities, setFacilities] = useState(STATIC_DATA_REPLACE_LATER);
+
+    //useEffect to fetch data from the backend will go here.
+
+
     const tableHeaders = [
         "Facility Name",
         "Address",
@@ -12,7 +38,6 @@ const FactoryTable = (props: { }) => {
         "OEEE",
         "Lastest Update"
     ];  
-    
     
     return (
     <div className="flex flex-col items-center justify-center mx-auto z-30">
@@ -26,7 +51,30 @@ const FactoryTable = (props: { }) => {
                 </tr>
             </thead>
             <tbody>
-
+            {facilities.length > 0 ? (
+                <>
+                    {facilities.map((facility) => (
+                        <tr className="flex-grow text-sm text-[#858A8F]">
+                            <td className="border px-4 py-2.5">{facility.name}</td>
+                            <td className="border px-4 py-2.5">{facility.address}</td>
+                            <td className="border px-4 py-2.5">{facility.lat}</td>
+                            <td className="border px-4 py-2.5">{facility.lon}</td>
+                            <td className="border px-4 py-2.5">{facility.city}</td>
+                            <td className="border px-4 py-2.5">{facility.state}</td>
+                            <td className="border px-4 py-2.5">{facility.country}</td>
+                            <td className="border px-4 py-2.5">{facility.OEEE}</td>
+                            <td className="border px-4 py-2.5">{facility.lastUpdate}</td>
+                        </tr>
+                        
+                    ))}
+                </>    
+            
+            ) : (
+                <tr className="flex-grow">
+                    <td className="border px-4 py-2 h-60 text-lg text-gray-400" colSpan={9}><i>No Facilities Found</i></td>
+                </tr>
+            )
+            }
             </tbody>
         </table>
     </div>
