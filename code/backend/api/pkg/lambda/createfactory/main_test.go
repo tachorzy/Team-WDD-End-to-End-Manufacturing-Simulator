259e229c13fb539fe0eeb226e93d8ab9edbc37a3
Bryant Le <bryantle2021@gmail.com>
2024-02-26 20:04:55 -0600

    test(api/createfactory): add bad json

--- code/backend/api/pkg/lambda/createfactory/main_test.go
+++ code/backend/api/pkg/lambda/createfactory/main_test.go
@@ -22,3 +22,20 @@ func TestSuccess(t *testing.T) {
 		t.Errorf("Expected StatusCode 200, got %d", response.StatusCode)
 	}
 }
+
+func TestBadJSON(t *testing.T) {
+	request := events.APIGatewayProxyRequest{
+		Body: `{"name":"Name", "location": "Invalid", "description":"Description"}`,
+	}
+
+	ctx := context.Background()
+	response, err := HandleRequest(ctx, request)
+
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	if response.StatusCode != 400 {
+		t.Errorf("Expected StatusCode 400 for bad JSON, got %d", response.StatusCode)
+	}
+}
