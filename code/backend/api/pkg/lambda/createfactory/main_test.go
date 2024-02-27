e326508eb5495aae9be0ea7f587ea576d1c20cac
Bryant Le <bryantle2021@gmail.com>
2024-02-26 20:09:05 -0600

    test(api/createfactory): remove success

--- code/backend/api/pkg/lambda/createfactory/main_test.go
+++ code/backend/api/pkg/lambda/createfactory/main_test.go
@@ -6,23 +6,6 @@ import (
 	"testing"
 )
 
-func TestSuccess(t *testing.T) {
-	request := events.APIGatewayProxyRequest{
-		Body: `{"name":"Name","location":{"longitude":1.0,"latitude":1.0},"description":"Description"}`,
-	}
-
-	ctx := context.Background()
-	response, err := HandleRequest(ctx, request)
-
-	if err != nil {
-		t.Fatalf("Expected no error, got %v", err)
-	}
-
-	if response.StatusCode != 200 {
-		t.Errorf("Expected StatusCode 200, got %d", response.StatusCode)
-	}
-}
-
 func TestBadJSON(t *testing.T) {
 	request := events.APIGatewayProxyRequest{
 		Body: `{"name":"Name", "location": "Invalid", "description":"Description"}`,
