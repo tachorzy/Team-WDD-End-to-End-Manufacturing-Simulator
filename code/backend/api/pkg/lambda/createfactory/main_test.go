3b9841b42b67ea391c8e41c7cc6107420ff5d7da
Bryant Le <bryantle2021@gmail.com>
2024-02-26 20:04:23 -0600

    test(api/createfactory): add success

---
+++ code/backend/api/pkg/lambda/createfactory/main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/events"
+	"testing"
+)
+
+func TestSuccess(t *testing.T) {
+	request := events.APIGatewayProxyRequest{
+		Body: `{"name":"Name","location":{"longitude":1.0,"latitude":1.0},"description":"Description"}`,
+	}
+
+	ctx := context.Background()
+	response, err := HandleRequest(ctx, request)
+
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	if response.StatusCode != 200 {
+		t.Errorf("Expected StatusCode 200, got %d", response.StatusCode)
+	}
+}
