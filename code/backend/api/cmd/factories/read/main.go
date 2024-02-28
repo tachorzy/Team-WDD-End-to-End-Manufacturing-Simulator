8ce62bd838f2740815837b4155891ee16da03fc3
Bryant Le <bryantle2021@gmail.com>
2024-02-27 22:18:06 -0600

    refactor: restructure readfactory

---
+++ code/backend/api/cmd/factories/read/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"wdd/api/internal/handlers/factories"
+)
+
+const AWSREGION = "us-east-2"
+
+func main() {
+	cfg, err := config.LoadDefaultConfig(context.Background(), config.WithRegion(AWSREGION))
+	if err != nil {
+		panic(fmt.Sprintf("Failed loading config, %v", err))
+	}
+
+	svc := dynamodb.NewFromConfig(cfg)
+	handler := factories.NewReadFactoryHandler(svc)
+
+	lambda.Start(handler.HandleReadFactoryRequest)
+}
