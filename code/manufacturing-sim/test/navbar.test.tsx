12b2033d707e832524df131daadb92af2edaf680
ReginaldGreen02 <rejogreen@gmail.com>
2024-02-27 22:06:12 -0600

    fix: removed commented test

--- code/manufacturing-sim/test/navbar.test.tsx
+++ code/manufacturing-sim/test/navbar.test.tsx
@@ -16,14 +16,14 @@ describe("Navbar Component", () => {
       }),
     }));
 
-    // Render the Navbar component
+    
     render(<Navbar pageId="/" />);
 
-    // Check if the logo is rendered
+    
     const logo = screen.getByAltText("brand");
     expect(logo).toBeInTheDocument();
 
-    // Check if the navigation links are rendered
+    
     const homeLink = screen.getByText("Home");
     const gatewaysLink = screen.getByText("Gateways");
     const assetsLink = screen.getByText("Assets");
@@ -32,17 +32,17 @@ describe("Navbar Component", () => {
     expect(gatewaysLink).toBeInTheDocument();
     expect(assetsLink).toBeInTheDocument();
 
-    // Check if the login link and SignUpButton are rendered
+    
     const loginLink = screen.getByText("Login");
     const signUpButton = screen.getByText("Sign Up");
 
     expect(loginLink).toBeInTheDocument();
     expect(signUpButton).toBeInTheDocument();
 
-    // Check the initial text color for Home link
+    
     expect(homeLink).toHaveClass("text-[#494949]");
 
-    // Check the styling of the link based on the pageId prop
+    
     expect(homeLink).toHaveClass("group text-lg font-medium");
     expect(gatewaysLink).toHaveClass("group text-lg font-medium");
     expect(assetsLink).toHaveClass("group text-lg font-medium");
