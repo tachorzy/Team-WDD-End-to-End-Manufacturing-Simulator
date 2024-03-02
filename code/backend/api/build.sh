c86b6df5cbd8561012d9e6e489f83777084fd8d6
Bryant Le <bryantle2021@gmail.com>
2024-03-02 16:57:04 -0600

    ci(jenkins): add stage fail after every command

--- code/backend/api/build.sh
+++ code/backend/api/build.sh
@@ -1,4 +1,11 @@
 go get ./...
+[ $? -eq 0 ] || exit 1
+
 go build -o build -v ./cmd/...
+[ $? -eq 0 ] || exit 1
+
 go test -v ./internal/... -coverprofile="build/coverage.out"
-go tool cover -func="build/coverage.out
+[ $? -eq 0 ] || exit 1
+
+go tool cover -func="build/coverage.out"
+[ $? -eq 0 ] || exit 1
