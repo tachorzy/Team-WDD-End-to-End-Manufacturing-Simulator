e45e0d182fb74eacecd96e9205df471c919bf43c
C. <cairohyfr@gmail.com>
2024-03-09 06:55:22 -0600

    fix: manually adding cors to the responses

--- code/frontend/app/api/factories/factoryAPI.tsx
+++ code/frontend/app/api/factories/factoryAPI.tsx
@@ -11,7 +11,7 @@ const requestOptions: RequestInit = {
 const getFactory = async (factoryId: string): Promise<Factory> => {
     try {
         const response = await fetch(
-            `${BASE_URL}/factories?factoryId=${factoryId}`,
+            `${BASE_URL}/factories?id=${factoryId}`,
             requestOptions,
         );
         if (!response.ok) {
@@ -19,7 +19,7 @@ const getFactory = async (factoryId: string): Promise<Factory> => {
                 `Failed to fetch factory with ID ${factoryId}: ${response.statusText}`,
             );
         }
-        return (await response.json()) as Factory;
+        return await response.json() as Factory;
     } catch (error) {
         console.error(`Failed to fetch factory with ID ${factoryId}:`, error);
         throw new Error(`Failed to fetch factory with ID ${factoryId}`);
@@ -38,7 +38,7 @@ const createFactory = async (newFactory: Factory): Promise<Factory> => {
                 `Failed to add new factory: ${response.statusText}`,
             );
         }
-        return (await response.json()) as Factory;
+        return await response.json() as Factory;
     } catch (error) {
         console.error("Failed to add new factory:", error);
         throw new Error("Failed to add new factory");
@@ -53,9 +53,7 @@ const getAllFactories = async (): Promise<Factory[]> => {
                 `Failed to fetch all factories: ${response.statusText}`,
             );
         }
-        const responseBody = (await response.json()) as ApiResponse;
-        const data = JSON.parse(responseBody.body) as Factory[];
-        return data;
+        return await response.json() as Factory[];
     } catch (error) {
         console.error("Failed to fetch all factories: ", error);
         throw new Error("Failed to fetch all factories.");
