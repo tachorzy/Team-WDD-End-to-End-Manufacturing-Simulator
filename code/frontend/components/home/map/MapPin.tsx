59d56289b5afe0149727e46d634fa02570e77b39
tachorzy <tachorzyad@gmail.com>
2024-03-20 00:49:25 -0500

    style: fixed padding bug on popup close button

--- code/frontend/components/home/map/MapPin.tsx
+++ code/frontend/components/home/map/MapPin.tsx
@@ -18,43 +18,45 @@ const MapPin: React.FC<PinProps> = ({
     description,
     link,
     icon,
-}) => (
-    <Marker key={key} position={position} icon={icon}>
-        <Popup className="w-56 py-2">
-            <div className="w-full">
-                <h3 className="font-bold text-base text-slate-600">{title}</h3>
-                <div className="flex flex-col gap-y-1">
-                    <div className="flex flex-row gap-x-1 border-b-2 border-slate-300 py-0">
-                        <Image
-                            src="/icons/location.svg"
-                            width={17}
-                            height={17}
-                            className="select-none"
-                            alt="error pin"
-                        />
-                        <p className="font-thin	text-slate-500 my-0 text-xs">{`${position.lat.toFixed(2)}°, ${position.lng.toFixed(2)}°`}</p>
+}) => { 
+    return (
+        <Marker key={key} position={position} icon={icon}>
+            <Popup className="w-56">
+                <div className="w-full">
+                    <h3 className="font-bold text-base text-slate-600">{title}</h3>
+                    <div className="flex flex-col gap-y-1">
+                        <div className="flex flex-row gap-x-1 border-b-2 border-slate-300 py-0">
+                            <Image
+                                src="/icons/location.svg"
+                                width={17}
+                                height={17}
+                                className="select-none"
+                                alt="error pin"
+                            />
+                            <p className="font-thin	text-slate-500 my-0 text-xs">{`${position.lat.toFixed(2)}°, ${position.lng.toFixed(2)}°`}</p>
+                        </div>
+                    </div>
+
+                    {description && (
+                        <p className="font-text-xs text-pretty">{description}</p>
+                    )}
+                    <div className="w-fit">
+                        {link && (
+                            <Link
+                                href={link}
+                                className="text-sm hover:text-MainBlue group"
+                            >
+                                View Factory
+                                <span className="text-lg font-bold pt-0.5 pl-0.5 group-hover:pl-1.5 duration-500">
+                                    ›
+                                </span>
+                            </Link>
+                        )}
                     </div>
                 </div>
-
-                {description && (
-                    <p className="font-text-xs text-pretty">{description}</p>
-                )}
-                <div className="w-fit">
-                    {link && (
-                        <Link
-                            href={link}
-                            className="text-sm hover:text-MainBlue group"
-                        >
-                            View Factory
-                            <span className="text-lg font-bold pt-0.5 pl-0.5 group-hover:pl-1.5 duration-500">
-                                ›
-                            </span>
-                        </Link>
-                    )}
-                </div>
-            </div>
-        </Popup>
-    </Marker>
-);
+            </Popup>
+        </Marker>
+    );
+}
 
 export default MapPin;
