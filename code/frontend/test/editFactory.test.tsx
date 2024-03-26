26ea10822c5518cfa63b2dcff74105d9c83cb9fc
ReginaldGreen02 <rejogreen@gmail.com>
2024-03-26 11:14:25 -0500

    feat: setting up testing for editFactory test

--- code/frontend/test/editFactory.test.tsx
+++ code/frontend/test/editFactory.test.tsx
@@ -7,7 +7,24 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import EditFactoryForm from "@/components/factorydashboard/editFactory";
 
+
+global.fetch = jest.fn(() =>
+    Promise.resolve({
+        ok: true,
+        json: () =>
+            Promise.resolve({
+                factoryId: "1",
+                message: "",
+            }),
+    }),
+) as jest.Mock;
+
 describe("Landing Page Component", () => {
-
-
+    beforeEach(() => {
+        (global.fetch as jest.Mock).mockClear();
+    });
+    
+    test("renders and its compontents correctly ", () => {
+        
+    });
 });
