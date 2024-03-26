be96516ce53f374fba908919e0266aa5dbdeabaa
ReginaldGreen02 <rejogreen@gmail.com>
2024-03-26 11:05:47 -0500

    feat: created test suite for EditFactiryForm

---
+++ code/frontend/test/editFactory.test.tsx
@@ -0,0 +1,13 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import EditFactoryForm from "@/components/factorydashboard/editFactory";
+
+describe("Landing Page Component", () => {
+
+
+});
