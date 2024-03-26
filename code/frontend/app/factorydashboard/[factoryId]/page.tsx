ed600eadca1c8a2c60635ef5fa9e3ac143c30a5c
tachorzy <tachorzyad@gmail.com>
2024-03-26 02:27:38 -0500

    feat: added FileUploadContainer

--- code/frontend/app/factorydashboard/[factoryId]/page.tsx
+++ code/frontend/app/factorydashboard/[factoryId]/page.tsx
@@ -2,15 +2,17 @@
 
 import Header from "@/components/factorydashboard/header";
 import Navbar from "@/components/Navbar/Navbar";
+import FileUploadContainer from "@/components/floorplan/FileUploadContainer";
 
 export default function FactoryDashboard() {
     return (
         <main className="flex flex-col bg-[#FAFAFA] min-h-screen mx-auto smooth-scroll">
-            <div className="flex flex-col bg-[url('/background/Grid.svg')] max-h-1/2 rounded-3xl bg-opacity-[15%]">
+            <div className="flex flex-col bg-[url('/background/Grid.svg')] min-h-screen rounded-3xl bg-opacity-[15%]">
                 <div className="px-32">
                     <Navbar pageId="Home" />
                     <div className="flex flex-col items-center justify-center gap-y-5 mt-16 mx-auto overflow-hidden max-h-screen">
                         <Header />
+                        <FileUploadContainer/>
                     </div>
                 </div>
             </div>
--- code/frontend/components/floorplan/FileUploadContainer.tsx
+++ code/frontend/components/floorplan/FileUploadContainer.tsx
@@ -1,13 +1,56 @@
 import React from "react";
+import { useDropzone } from 'react-dropzone'
 import Image from "next/image";
 
 const FileUploadContainer = () => {
+    const {
+        acceptedFiles,
+        fileRejections,
+        getRootProps,
+        getInputProps
+      } = useDropzone({
+        accept: {
+          'image/jpeg': [],
+          'image/png': []
+        }
+      });
+    
 
-    return (
-        <div>
-
-        </div>
-    );
-};
+    const acceptedFileItems = (acceptedFiles as DropzoneFile[]).map(file => (
+        <li key={file.path}>
+            {file.path} - {file.size} bytes
+        </li>
+    ));
+    
+      const fileRejectionItems = fileRejections.map(({ file, errors }) => (
+        <li key={file.path}>
+          {file.path} - {file.size} bytes
+          <ul>
+            {errors.map(e => (
+              <li key={e.code}>{e.message}</li>
+            ))}
+          </ul>
+        </li>
+      ));
+    
+      return (
+        <section className="w-full h-full items-center justify-center">
+          <div {...getRootProps({ className: 'dropzone' })} className="group flex flex-col w-[62.5%] h-[30rem] p-10 gap-y-6 items-center justify-center border-MainBlue hover:border-LightBlue transition duration-700 ease-in border-dashed border-4 rounded-2xl cursor-pointer scale-[100.25%] shadow-sm">
+            <input {...getInputProps()} />
+            <Image width={90} height={90} src="/factorydashboard/upload.svg" alt="upload icon" className="group-hover:animate-pulse-slow transition duration-1000 ease-out"></Image>
+            <div>
+                <p className="text-slate-500 text-lg font-medium text-center">Click or drop your floor plan file here to upload.</p>
+                <em className="text-slate-700 text-lg font-medium text-center"><span className="text-red-400">*</span>Files supported: .png, .jpg, .svg, .glb</em>
+            </div>
+          </div>
+          <div>
+            <h4 className="text-DarkBlue text-sm">Accepted files</h4>
+                <ul className="text-DarkBlue text-sm">{acceptedFileItems}</ul>
+            <h4 className="text-DarkBlue text-sm">Rejected files</h4>
+                <ul className="text-DarkBlue text-sm">{fileRejectionItems}</ul>
+          </div>
+        </section>
+      );
+    };
 
 export default FileUploadContainer;
