2a6560b431679b56d5c19e20b1f67eb852f5bd17
tachorzy <tachorzyad@gmail.com>
2024-03-26 00:12:34 -0500

    chore: installed react-dropzone

---
+++ code/frontend/components/floorplan/FileUploadContainer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Image from "next/image";
+
+const FileUploadContainer = () => {
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default FileUploadContainer;
