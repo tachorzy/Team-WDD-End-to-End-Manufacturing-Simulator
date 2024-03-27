adc13672b39e97809fc442c0d9e0f0b79d5e4185
tachorzy <tachorzyad@gmail.com>
2024-03-26 23:58:31 -0500

    test: added test for invalid file uploads

--- code/frontend/test/fileuploadcontainer.test.tsx
+++ code/frontend/test/fileuploadcontainer.test.tsx
@@ -36,4 +36,38 @@ describe('FileUploadContainer', () => {
         expect(useDropzone).toHaveBeenCalled();
         expect(getByText(new RegExp(`image.${fileType} - 5000 bytes`))).not.toBeNull();
     });
+
+    test('should reject invalid files', () => {
+        const mockRejectedFiles = [{
+            file: {
+                path: 'invalidfile.txt',
+                size: 5000,
+                type: 'text/plain',
+            },
+            errors: [{
+                code: 'file-invalid-type',
+                message: 'File type must be image/jpeg, image/png, or image/svg+xml',
+            }],
+        }];
+
+        (useDropzone as jest.Mock).mockReturnValue({
+            getRootProps: () => ({}),
+            getInputProps: () => ({}),
+            acceptedFiles: [],
+            fileRejections: mockRejectedFiles,
+        });
+
+        const { getByText } = render(<FileUploadContainer />);
+        const input = getByText(/Click or drop your floor plan file here to upload./).parentElement;
+        const dropEvent = createEvent.drop(input as Element);
+        Object.defineProperty(dropEvent, 'dataTransfer', {
+          value: {
+            files: mockRejectedFiles,
+          },
+        });
+        fireEvent(input as Element, dropEvent);
+
+        expect(useDropzone).toHaveBeenCalled();
+        expect(getByText(/invalidfile.txt - 5000 bytes/)).not.toBeNull();
+    });
 });
