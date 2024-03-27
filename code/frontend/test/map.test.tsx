bfd516117c2ec8a6335334e79cf3cf0634706138
beansalsa <wyatt.fernandez101@gmail.com>
2024-03-26 20:28:28 -0500

    Update: Fetch Error Test

--- code/frontend/test/map.test.tsx
+++ code/frontend/test/map.test.tsx
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import MapComponent from "../components/home/map/Map.client";
 
 jest.mock("react-leaflet", () => ({
@@ -26,7 +26,34 @@ const mockFactories = [
 ];
 
 describe ("MapCompenent", () => {
+    beforeEach(() => {
+        global.fetch = jest.fn(() => 
+            Promise.resolve({  
+                json: () => Promise.resolve(
+                    {
+                        mockFactories,
+                    },
+                ),
+            })
+        ) as jest.Mock;
+    });
+
+    afterEach(() => {
+        (global.fetch as jest.Mock).mockClear();
+    });
+
     test("renders without error", () => {
         render(<MapComponent positions={mockFactories}/>);
     });
+
+    test("error occurs on fetch", async () => {
+        (global.fetch as jest.Mock).mockImplementationOnce(() => Promise.reject(new Error("404")));        
+        const consoleErrorMock = jest.spyOn(console, "error").mockImplementation(() => {});
+
+        render(<MapComponent positions={mockFactories} />);
+
+        await waitFor(() => {
+            expect(consoleErrorMock).toHaveBeenCalledWith("Error fetching factories:", new Error("404"));
+        });
+    })
 });
