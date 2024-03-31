31dd8deb508f8e70180637f1aaf71f72cbacebae
Jaq-Medina <jaquelinemedina453@yahoo.com>
2024-03-30 20:22:08 -0500

    Update Model function

---
+++ code/backend/api/internal/handlers/models/updatemodel.go
@@ -0,0 +1,90 @@
+package models
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+)
+
+func NewUpdateModelHandler(db DynamoDBClient) *Handler {
+	return &Handler{
+		DynamoDB: db,
+	}
+}
+
+var UpdateExpressionBuilder = func(update expression.UpdateBuilder) (expression.Expression, error) {
+	return expression.NewBuilder().WithUpdate(update).Build()
+}
+
+func (h Handler) HandleUpdateModelRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var model Model
+	headers := map[string]string{
+		"Access-Control-Allow-Origin": "*",
+		"Content-Type":                "application/json",
+	}
+
+	if err := json.Unmarshal([]byte(request.Body), &model); err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusBadRequest,
+			Headers:    headers,
+			Body:       fmt.Sprintf("Error parsing JSON body: %s", err.Error()),
+		}, nil
+	}
+
+	key := map[string]types.AttributeValue{
+		"modelId": &types.AttributeValueMemberS{Value: model.ModelID},
+	}
+
+	var updateBuilder expression.UpdateBuilder
+
+	if model.Attributes != nil {
+		// Dereference the pointer to access the underlying slice
+		for _, attr := range *model.Attributes {
+			updateBuilder = updateBuilder.Add(expression.Name("attributes"), expression.Value(attr))
+		}
+	}
+
+	if model.Properties != nil {
+		for _, prop := range *model.Properties {
+			updateBuilder = updateBuilder.Add(expression.Name("properties"), expression.Value(prop))
+		}
+	}
+
+	expr, err := UpdateExpressionBuilder(updateBuilder)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Headers:    headers,
+			Body:       fmt.Sprintf("Failed to build update expression: %s", err.Error()),
+		}, nil
+	}
+
+	input := &dynamodb.UpdateItemInput{
+		Key:                       key,
+		TableName:                 aws.String(TABLENAME),
+		ExpressionAttributeNames:  expr.Names(),
+		ExpressionAttributeValues: expr.Values(),
+		UpdateExpression:          expr.Update(),
+	}
+
+	if _, err = h.DynamoDB.UpdateItem(ctx, input); err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Headers:    headers,
+			Body:       fmt.Sprintf("Error updating item into DynamoDB: %s", err.Error()),
+		}, nil
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Headers:    headers,
+		Body:       fmt.Sprintf("Model with ID %s updated successfully", model.ModelID),
+	}, nil
+}
