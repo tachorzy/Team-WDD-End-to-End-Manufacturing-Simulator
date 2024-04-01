7bb0eee80e58f82121b8d9de9b4fdfa99a26eaa4
tachorzy <tachorzyad@gmail.com>
2024-03-31 20:51:29 -0500

    test: added test cases for FactoryPageNavbar

---
+++ code/frontend/test/factorypagenavbar.test.tsx
@@ -0,0 +1,51 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import FactoryPageNavbar from "../components/Navbar/FactoryPageNavbar";
+
+describe("FactoryPageNavBar", () => {
+    test("renders Navbar component with correct links and buttons", () => {
+        jest.mock("next/router", () => ({
+            useRouter: () => ({
+                pathname: "/",
+            }),
+        }));
+
+        render(<FactoryPageNavbar pageId="/" factoryId="123456789" />);
+
+        const logo = screen.getByAltText("brand");
+        expect(logo).toBeInTheDocument();
+
+        const homeLink = screen.getByText("Home");
+        const dashboardLink = screen.getByText("Dashboard");
+        const assetsLink = screen.getByText("Assets");
+        const anomalyDetectionLink = screen.getByText("Anomaly Detection");
+
+        expect(homeLink).toBeInTheDocument();
+        expect(dashboardLink).toBeInTheDocument();
+        expect(assetsLink).toBeInTheDocument();
+        expect(anomalyDetectionLink).toBeInTheDocument();
+
+        const loginLink = screen.getByText("Login");
+        const signUpButton = screen.getByText("Sign Up");
+
+        expect(loginLink).toBeInTheDocument();
+        expect(signUpButton).toBeInTheDocument();
+
+        expect(homeLink).toHaveClass("text-[#494949]");
+
+        expect(homeLink).toHaveClass("text-[#494949] group text-base font-medium -mt-4");
+        expect(dashboardLink).toHaveClass("group text-lg font-medium");
+        expect(assetsLink).toHaveClass("group text-lg font-medium");
+        expect(anomalyDetectionLink).toHaveClass("group text-lg font-medium");
+
+        expect(loginLink).toHaveClass("group text-lg font-medium");
+        expect(signUpButton).toHaveClass(
+            "text-sm font-medium text-[#494949] py-0.5 px-2",
+        );
+    });
+});
