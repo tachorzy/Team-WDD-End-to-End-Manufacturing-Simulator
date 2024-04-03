45389e6c1ab97cf49d70c74cc2c85eaf549cb802
C. <cairohyfr@gmail.com>
2024-04-02 21:17:48 -0500

    fix build error

--- code/frontend/app/api/floorplan/floorplanAPI.ts
+++ code/frontend/app/api/floorplan/floorplanAPI.ts
@@ -29,12 +29,20 @@ export const createFloorplan = async (
     }
 };
 
-export const getFloorplan = async (id: string): Promise<Floorplan> => {
+export const getFloorplan = async (id: string): Promise<Floorplan | null> => {
     try {
         const response = await fetch(`${BASE_URL}/floorplan/?id=${id}`);
+        if (!response.ok) {
+            if (response.status === 404) {
+                console.log(`No floorplan found for factory ID ${id}`);
+                return null;
+            }
+            throw new Error(`Failed to get floorplan: ${response.statusText}`);
+        }
         return (await response.json()) as Floorplan;
     } catch (error) {
         console.error("Failed to get floorplan", error);
+        throw new Error("Failed to get floorplan");
     
     }
 }
