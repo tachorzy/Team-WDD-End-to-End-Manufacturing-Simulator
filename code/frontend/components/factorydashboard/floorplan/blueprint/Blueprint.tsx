c78a941f9d63b05c49bbb54972b01f70ab2c6f48
tachorzy <tachorzyad@gmail.com>
2024-04-03 04:50:36 -0500

    fix: fixed the blueprint height bounds not exceeding the size of the image

--- code/frontend/components/factorydashboard/floorplan/blueprint/Blueprint.tsx
+++ code/frontend/components/factorydashboard/floorplan/blueprint/Blueprint.tsx
@@ -7,7 +7,7 @@ const Blueprint = (props: { imageFile: File }) => {
     const imageURL = URL.createObjectURL(imageFile);
 
     return (
-        <div className="sticky overflow-hidden max-h-min w-[55%]">
+        <div className="sticky overflow-hidden h-min w-[55%]">
             <AssetMarker />
             <Image
                 src={imageURL}
@@ -15,7 +15,7 @@ const Blueprint = (props: { imageFile: File }) => {
                 height={775}
                 quality={100}
                 alt="upload result tray"
-                className="select-none mb-2 z-0"
+                className="select-none z-0"
             />
         </div>
     );
