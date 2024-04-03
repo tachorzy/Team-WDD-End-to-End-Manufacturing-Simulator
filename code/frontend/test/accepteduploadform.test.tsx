69eec2fde4b01cd94bd0dfacb6212f6b6af1ba87
beansalsa <wyatt.fernandez101@gmail.com>
2024-04-03 05:08:49 -0500

    Update: lint

--- code/frontend/test/accepteduploadform.test.tsx
+++ code/frontend/test/accepteduploadform.test.tsx
@@ -10,13 +10,13 @@ const mockUsePathname = jest.fn();
 const mockCreateFloorplan = jest.fn();
 
 jest.mock("next/navigation", () => ({
-    usePathname(): string{
+    usePathname(): string {
         return mockUsePathname();
     },
 }));
 
 jest.mock("@/app/api/floorplan/floorplanAPI", () => ({
-    createFloorplan(): Promise<string>{
+    createFloorplan(): Promise<string> {
         return mockCreateFloorplan();
     },
 }));
@@ -25,14 +25,16 @@ const consoleSpy = jest.spyOn(console, "error").mockImplementation(() => {});
 
 const mockSetUploadedFile = jest.fn();
 const mockSetFloorPlanFile = jest.fn();
-const mockReaderResult = jest.spyOn(global.FileReader.prototype, "result", "get");
+const mockReaderResult = jest.spyOn(
+    global.FileReader.prototype,
+    "result",
+    "get",
+);
 
 const props = {
-    uploadedFile: new File(["blobpart"], "image.jpg", { type: `image.jpg` }),
+    uploadedFile: new File(["blobpart"], "image.jpg", { type: "image.jpg" }),
     setUploadedFile: mockSetUploadedFile,
-    acceptedFileItems: [
-        <li key="image.jpg">image.jpg - 8 bytes</li>,
-    ],
+    acceptedFileItems: [<li key="image.jpg">image.jpg - 8 bytes</li>],
     fileRejectionItems: [
         <li key="image.jpg">
             image.jpg - 8 bytes
@@ -56,7 +58,9 @@ describe("AcceptedUploadForm", () => {
     });
 
     test("should render with no errors", () => {
-        const { getByAltText, getByTestId, getByText } = render(<AcceptedUploadForm {...props} />);
+        const { getByAltText, getByTestId, getByText } = render(
+            <AcceptedUploadForm {...props} />,
+        );
 
         expect(getByAltText("close icon")).toBeInTheDocument();
         expect(getByAltText("Uploaded content")).toBeInTheDocument();
@@ -66,14 +70,16 @@ describe("AcceptedUploadForm", () => {
     });
 
     test("should close the modal when close icon is clicked", () => {
-        const { getByAltText, getByTestId, getByText } = render(<AcceptedUploadForm {...props}/>);
+        const { getByAltText, getByTestId, getByText } = render(
+            <AcceptedUploadForm {...props} />,
+        );
 
-        const closeIcon = getByAltText("close icon")
-        const uploadContent = getByAltText("Uploaded content")
-        const uploadResultTray = getByTestId("upload-result-tray")
-        const accept = getByText("Accept")
-        const deny = getByText("Deny")
-        
+        const closeIcon = getByAltText("close icon");
+        const uploadContent = getByAltText("Uploaded content");
+        const uploadResultTray = getByTestId("upload-result-tray");
+        const accept = getByText("Accept");
+        const deny = getByText("Deny");
+
         act(() => {
             closeIcon.click();
         });
@@ -84,28 +90,27 @@ describe("AcceptedUploadForm", () => {
         expect(accept).not.toBeInTheDocument();
         expect(deny).not.toBeInTheDocument();
         expect(mockSetUploadedFile).toHaveBeenCalledWith(null);
-
     });
 
     test("should call createFloorplan when Accept button is clicked", async () => {
         mockCreateFloorplan.mockResolvedValue("success");
         mockUsePathname.mockReturnValueOnce("some-path/some-path/1");
-        mockReaderResult.mockImplementationOnce(() => "some-data,base64Image")
-        const { getByText } = render(<AcceptedUploadForm {...props}/>);
+        mockReaderResult.mockImplementationOnce(() => "some-data,base64Image");
+        const { getByText } = render(<AcceptedUploadForm {...props} />);
 
         act(() => {
             getByText("Accept").click();
             expect(mockSetUploadedFile).toHaveBeenCalledWith(null);
         });
 
-        await waitFor(()=> {
+        await waitFor(() => {
             expect(mockCreateFloorplan).toHaveBeenCalledTimes(1);
             expect(mockSetUploadedFile).toHaveBeenCalledWith(null);
         });
     });
 
     test("should call not createFloorplan when Deny button is clicked", () => {
-        const { getByText } = render(<AcceptedUploadForm {...props}/>);
+        const { getByText } = render(<AcceptedUploadForm {...props} />);
 
         act(() => {
             getByText("Deny").click();
@@ -117,74 +122,81 @@ describe("AcceptedUploadForm", () => {
 
     test("should log error when factoryID is missing", async () => {
         mockUsePathname.mockReturnValueOnce("some-path/some-path");
-        const { getByText } = render(<AcceptedUploadForm {...props}/>);
+        const { getByText } = render(<AcceptedUploadForm {...props} />);
 
         act(() => {
             getByText("Accept").click();
         });
 
         await waitFor(() => {
-            expect(consoleSpy).toHaveBeenCalledWith("File or Factory ID is missing.");
+            expect(consoleSpy).toHaveBeenCalledWith(
+                "File or Factory ID is missing.",
+            );
         });
     });
 
     test("should log error when base64image is missing", async () => {
         mockUsePathname.mockReturnValueOnce("some-path/some-path/1");
-        mockReaderResult.mockImplementationOnce(() => null)
-        const { getByText } = render(<AcceptedUploadForm {...props}/>);
+        mockReaderResult.mockImplementationOnce(() => null);
+        const { getByText } = render(<AcceptedUploadForm {...props} />);
 
         act(() => {
             getByText("Accept").click();
             expect(mockSetUploadedFile).toHaveBeenCalledWith(null);
         });
 
-        await waitFor(()=> {
-            expect(consoleSpy).toHaveBeenCalledWith("Failed to convert the file to base64.");
+        await waitFor(() => {
+            expect(consoleSpy).toHaveBeenCalledWith(
+                "Failed to convert the file to base64.",
+            );
         });
-    }); 
+    });
 
     test("should log error when createFloorplan fails", async () => {
         mockUsePathname.mockReturnValueOnce("some-path/some-path/1");
-        mockReaderResult.mockImplementationOnce(() => "some-data,base64Image")
+        mockReaderResult.mockImplementationOnce(() => "some-data,base64Image");
         mockCreateFloorplan.mockRejectedValueOnce(new Error("404"));
-        const { getByText } = render(<AcceptedUploadForm {...props}/>);
+        const { getByText } = render(<AcceptedUploadForm {...props} />);
 
         act(() => {
             getByText("Accept").click();
             expect(mockSetUploadedFile).toHaveBeenCalledWith(null);
         });
 
-        await waitFor(()=> {
-            expect(consoleSpy).toHaveBeenCalledWith("Error uploading floor plan:", new Error("404"));
+        await waitFor(() => {
+            expect(consoleSpy).toHaveBeenCalledWith(
+                "Error uploading floor plan:",
+                new Error("404"),
+            );
         });
     });
 
     test("should log error when there is an error reading the file", async () => {
         mockUsePathname.mockReturnValueOnce("some-path/some-path/1");
-        mockReaderResult.mockImplementationOnce(() => "some-data,base64Image")
+        mockReaderResult.mockImplementationOnce(() => "some-data,base64Image");
 
         // mockReadAsDataURL.mockImplementationOnce(() => {setTimeout(() => (this as any).onerror(), 0)});
 
-        window.FileReader = jest.fn().mockImplementation(() => {
-            return {
-                readAsDataURL: function() {
-                    setTimeout(() => this.onerror(new Error('Mock error')), 0);
-                },
-                EMPTY: 0,
-                LOADING: 1,
-                DONE: 2,
-            };
-        }) as unknown as typeof FileReader;
+        window.FileReader = jest.fn().mockImplementation(() => ({
+            readAsDataURL() {
+                setTimeout(() => this.onerror(new Error("Mock error")), 0);
+            },
+            EMPTY: 0,
+            LOADING: 1,
+            DONE: 2,
+        })) as unknown as typeof FileReader;
 
-        const { getByText } = render(<AcceptedUploadForm {...props}/>);
+        const { getByText } = render(<AcceptedUploadForm {...props} />);
 
         act(() => {
-                getByText("Accept").click();
-                expect(mockSetUploadedFile).toHaveBeenCalledWith(null);
+            getByText("Accept").click();
+            expect(mockSetUploadedFile).toHaveBeenCalledWith(null);
         });
 
-        await waitFor(()=> {
-            expect(consoleSpy).toHaveBeenCalledWith("There was an error reading the file");
+        await waitFor(() => {
+            expect(consoleSpy).toHaveBeenCalledWith(
+                "There was an error reading the file",
+            );
         });
     });
 });
