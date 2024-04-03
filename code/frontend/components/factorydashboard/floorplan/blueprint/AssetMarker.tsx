23708a26f20450f5ac375fe1e9be0f4e3ff678db
tachorzy <tachorzyad@gmail.com>
2024-04-03 05:09:24 -0500

    style: styled marker label

--- code/frontend/components/factorydashboard/floorplan/blueprint/AssetMarker.tsx
+++ code/frontend/components/factorydashboard/floorplan/blueprint/AssetMarker.tsx
@@ -12,7 +12,7 @@ const AssetMarker: React.FC<AssetMarkerProps> = (props) => {
 
     return (
         <Draggable>
-            <div className="group absolute top-0 left-0 z-10 drop-shadow-md">
+            <div className="group flex flex-col absolute top-0 left-0 z-10 drop-shadow-md items-center">
                 <Image
                     src="/icons/floorplan/asset-marker.svg"
                     width={30}
@@ -21,7 +21,7 @@ const AssetMarker: React.FC<AssetMarkerProps> = (props) => {
                     onDragStart={(e) => e.preventDefault()}
                     className="select-none hover:cursor-grabbing"
                 />
-                <p className="shadow-md text-xs group-hover:visible invisible text-center self-center">{asset?.name}</p>
+                <p className="shadow-md text-xs group-hover:visible invisible text-center self-center bg-opacity-[40%] px-1 py-0.5 rounded-sm bg-gray-800 my-1">{asset?.name}</p>
             </div>
         </Draggable>
 )};
