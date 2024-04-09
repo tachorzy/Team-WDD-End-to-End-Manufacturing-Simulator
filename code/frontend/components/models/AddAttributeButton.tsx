6b02215b3230d70e28aa08cf25ed8f050b6b6814
tachorzy <tachorzyad@gmail.com>
2024-04-09 00:43:25 -0500

    feat: added AddAttributeButton and AddPropertyButton

--- code/frontend/components/models/AddAttributeButton.tsx
+++ code/frontend/components/models/AddAttributeButton.tsx
@@ -1,26 +1,18 @@
 import React from "react";
 import Image from "next/image";
 
-type Attribute = { attribute: string; value: string };
-type Property = { property: string; unit: string };
-
-const AddedModelCharacteristic = (props: {
-    characteristic: string;
-    setCharacteristic:
-        | React.Dispatch<React.SetStateAction<[{ attribute: ""; value: "" }]>>
-        | React.Dispatch<React.SetStateAction<[{ property: ""; unit: "" }]>>;
+const AddAttributeButton = (props: {
+    setAttributes: React.Dispatch<React.SetStateAction<{attribute: string, value: string}[]>>
 }) => {
-    const { characteristic, setCharacteristic } = props;
+    const { setAttributes } = props;
 
     return (
         <button
             type="button"
             onClick={() => {
-                setCharacteristic((prev: Array<Attribute | Property>) =>
-                    characteristic === "Attribute"
-                        ? [...prev, { attribute: "", value: "" }]
-                        : [...prev, { property: "", unit: "" }],
-                );
+                setAttributes((prev) => {
+                    return [...prev, { attribute: "", value: "" }];
+                });
             }}
             className="flex flex-row items-center justify-center cursor-pointer w-[96%] p-2 border-2 border-dashed border-[#494949] border-solid rounded-lg text-[#494949] hover:scale-[101.5%] hover:border-MainBlue transition duration-500 ease-in-out"
         >
@@ -32,10 +24,10 @@ const AddedModelCharacteristic = (props: {
                     alt="add symbol"
                 />
                 <h1 className="text-sm font-medium text-[#494949] py-0.5 px-2">
-                    Add {characteristic}
+                    Add Attribute
                 </h1>
             </div>
         </button>
     );
 };
-export default AddedModelCharacteristic;
+export default AddAttributeButton;
---
+++ code/frontend/components/models/AddPropertyButton.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Image from "next/image";
+
+const AddPropertyButton = (props: {
+    setProperties: React.Dispatch<React.SetStateAction<{property: string, unit: string}[]>>
+}) => {
+    const { setProperties } = props;
+
+    return (
+        <button
+            type="button"
+            onClick={() => {
+                setProperties((prev) => {
+                    return [...prev, { property: "", unit: "" }];
+                });
+            }}
+            className="flex flex-row items-center justify-center cursor-pointer w-[96%] p-2 border-2 border-dashed border-[#494949] border-solid rounded-lg text-[#494949] hover:scale-[101.5%] hover:border-MainBlue transition duration-500 ease-in-out"
+        >
+            <div className="flex flex-row">
+                <Image
+                    src="/icons/add.svg"
+                    width={16}
+                    height={16}
+                    alt="add symbol"
+                />
+                <h1 className="text-sm font-medium text-[#494949] py-0.5 px-2">
+                    Add Property
+                </h1>
+            </div>
+        </button>
+    );
+};
+export default AddPropertyButton;
