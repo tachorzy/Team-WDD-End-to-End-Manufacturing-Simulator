bcd955069951531e3af02ae5080b510f5c32239f
beansalsa <wyatt.fernandez101@gmail.com>
2024-04-10 18:03:18 -0500

    Update: new tests + refactor

--- code/frontend/test/assetuploadcontainer.test.tsx
+++ code/frontend/test/assetuploadcontainer.test.tsx
@@ -3,107 +3,126 @@
  */
 import React from "react";
 import "@testing-library/jest-dom";
-import { createEvent, fireEvent, render, act} from "@testing-library/react"; // Import screen here
-import {
-    useDropzone,
-    DropzoneRootProps,
-    DropzoneInputProps,
-} from "react-dropzone";
+import { fireEvent, render, renderHook, waitFor, act} from "@testing-library/react";
 import  FileUploadContainer from "../components/factorydashboard/floormanager/assetform/AssetUploadContainer"
+import { Asset } from "@/app/api/_utils/types";
 
-jest.mock('react-dropzone', () => ({
-    useDropzone: jest.fn(),
-  }));
+const consoleErrorMock = jest
+    .spyOn(console, "error")
+    .mockImplementation(() => {});
 
+const mocksetAssetImageFile = jest.fn();
+const mocksetFormData = jest.fn().mockImplementation();
+
+const props = {
+    setAssetImageFile: mocksetAssetImageFile,
+    setFormData: mocksetFormData,
+}
 
 describe("AssetUploadContainer", () => {
     afterEach(() => {
         jest.clearAllMocks();
     });
 
-    test("should handle no file being dropped", () => {
-        (useDropzone as jest.Mock).mockReturnValue({
-            getRootProps: () => ({}),
-            getInputProps: () => ({}),
-            acceptedFiles: [],
-            fileRejections: [],
-        });
-        const { getByText } = render(
-            <FileUploadContainer setAssetImageFile={jest.fn()} setFormData={jest.fn()}/>,
-        );
+    test("should render without errors", () => {
+        const { getByTestId, getByAltText, getByText } = render(<FileUploadContainer {...props}/>)
 
-        const input = getByText(
-            /Click or drop your asset image here to upload./,
-        ).parentElement;
-        const dropEvent = createEvent.drop(input as Element);
-        Object.defineProperty(dropEvent, "dataTransfer", {
-            value: {
-                files: [],
-            },
-        });
-        fireEvent(input as Element, dropEvent);
-
-        expect(useDropzone).toHaveBeenCalled();
-        expect(input).not.toBeNull();
-
+        expect(getByTestId("dropzone")).toBeInTheDocument();
+        expect(getByTestId("drop-input")).toBeInTheDocument();
+        expect(getByAltText("upload icon")).toBeInTheDocument();
+        expect(getByText("Click or drop your asset image here to upload.")).toBeInTheDocument();
     });
 
-    test("should handle file being accepted", () => {
-        const setAssetImageFile = jest.fn();
-
-        (useDropzone as jest.Mock).mockReturnValue({
-            getRootProps: jest.fn() as jest.Mock<DropzoneRootProps>,
-            getInputProps: jest.fn() as jest.Mock<DropzoneInputProps>,
-            acceptedFiles: [
-                new File(["test content"], "test.png", {
-                    type: "image/png",
-                }),
-            ],
-            fileRejections: [],
-        });
-
-        render(
-            <FileUploadContainer setAssetImageFile={jest.fn()} setFormData={jest.fn()}/>,
-        );
-
+    test.each([
+        ["jpeg", "jpeg"],
+        ["png", "png"],
+        ["svg", "svg+xml"],
+    ])("should handle %s file drop", async (filename, filetype) => {
+        jest.spyOn(
+            global.FileReader.prototype,
+            "result",
+            "get",
+        )
+        .mockImplementationOnce(() => "some-data,base64String");
         
+        const { getByTestId } = render(<FileUploadContainer {...props}/>);
 
-        setTimeout(() => {
-           
-            expect(setAssetImageFile).toHaveBeenCalledWith(
-                expect.any(File),
-            );
-        }, 0);
-    });
-    
-/*
-    test('handles file drop', () => {
-        const setAssetImageFileMock = jest.fn();
-        const setFormDataMock = jest.fn();
-        const mockDropzone = { getRootProps: jest.fn(), getInputProps: jest.fn(), open: jest.fn() };
-        (useDropzone as jest.Mock).mockReturnValue(mockDropzone);
-      
-        render(<FileUploadContainer setAssetImageFile={setAssetImageFileMock} setFormData={setFormDataMock}/>);
-      
-        const file = new File(['(⌐□_□)'], 'chucknorris.png', { type: 'image/png' });
-        const dataTransfer = {
-          files: [file],
-          items: [
-            {
-              kind: 'file',
-              type: file.type,
-              getAsFile: () => file,
-            },
-          ],
-          types: ['Files'],
-        };
-      
-        act(() => {
-          fireEvent.drop(window, new DragEvent('drop', { dataTransfer }));
+        const file = new File(["blob"], `floorplan.${filename}`, { type: `image/${filetype}` });
+
+        const dropInput = getByTestId("drop-input");
+        Object.defineProperty(dropInput, "files", {
+            value: [file],
         });
-      
-        expect(useDropzone).toHaveBeenCalled();
-        expect(setAssetImageFileMock).toHaveBeenCalledWith(file);
-      });*/
+        fireEvent.drop(dropInput);  
+        
+        await waitFor(() => {
+            const updateFunction = mocksetFormData.mock.calls[0][0];
+            const prevData = { imageData: "" };
+            const newData = updateFunction(prevData);
+            expect(newData.imageData).toBe("base64String");
+            expect(mocksetAssetImageFile).toHaveBeenCalledWith(file);
+            expect(mocksetFormData).toHaveBeenCalledWith(expect.any(Function));
+        });
+    });
+
+    test("should not accept files other than jpeg, png, svg", async () => {
+        const { getByTestId } = render(<FileUploadContainer {...props}/>);
+
+        const file = new File(["blob"], "floorplan.pdf", { type: "application/pdf" });
+
+        const dropInput = getByTestId("drop-input");
+        Object.defineProperty(dropInput, "files", {
+            value: [file],
+        });
+        fireEvent.drop(dropInput);
+        
+        await waitFor(() => {
+            expect(mocksetAssetImageFile).toHaveBeenCalledWith(undefined);
+            expect(mocksetFormData).not.toHaveBeenCalled();
+            expect(consoleErrorMock).toHaveBeenCalled();
+        });
+    });
+
+    test("should not accept files larger than 8MB", async () => {
+        const { getByTestId } = render(<FileUploadContainer {...props}/>);
+
+        const file = new File([new ArrayBuffer(8000001)], "floorplan.jpeg", { type: "image/jpeg" });
+
+        const dropInput = getByTestId("drop-input");
+        Object.defineProperty(dropInput, "files", {
+            value: [file],
+        });
+        fireEvent.drop(dropInput);
+        
+        await waitFor(() => {
+            expect(mocksetAssetImageFile).toHaveBeenCalledWith(undefined);
+            expect(mocksetFormData).not.toHaveBeenCalled();
+            expect(consoleErrorMock).toHaveBeenCalled();
+        });
+    });
+
+    test("should handle file reader result being null", async () => {
+        jest.spyOn(
+                global.FileReader.prototype,
+                "result",
+                "get",
+            )
+            .mockImplementationOnce(() => null);
+
+        const { getByTestId } = render(<FileUploadContainer {...props}/>);
+
+        const file = new File(["blob"], "floorplan.png", { type: "image/png" });
+
+        const dropInput = getByTestId("drop-input");
+        Object.defineProperty(dropInput, "files", {
+            value: [file],
+        });
+        fireEvent.drop(dropInput);
+        
+        await waitFor(() => {
+            expect(mocksetAssetImageFile).toHaveBeenCalledWith(file);
+            expect(mocksetFormData).toHaveBeenCalledWith(expect.any(Function));
+        });
+    });
 });
 
