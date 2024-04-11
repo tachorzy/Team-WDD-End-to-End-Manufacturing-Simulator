c73e639116801b2eaa46703f52346f6618059236
tachorzy <tachorzyad@gmail.com>
2024-04-11 02:56:03 -0500

    refactor: extracted ModelIdField code into its own component

--- code/frontend/components/models/createmodelform/CreateModelForm.tsx
+++ code/frontend/components/models/createmodelform/CreateModelForm.tsx
@@ -4,7 +4,7 @@ import AttributeInputColumn from "./AttributeInputColumn";
 import PropertyInputColumn from "./PropertyInputColumn";
 import AddAttributeButton from "./AddAttributeButton";
 import AddPropertyButton from "./AddPropertyButton";
-
+import ModelIdField from "./ModelIdField";
 // interface CreateModelFormProps {
 //     onClose: () => void;
 //     onSave: (formData: Partial<Factory>) => void;
@@ -27,22 +27,7 @@ const CreateModelForm = (props: { factoryId: string }) => {
                 </h1>
                 <div className="flex flex-row gap-x-16 mt-6">
                     <section className="flex flex-col gap-y-3 min-w-max">
-                        <h1 className="text-2xl font-semibold text-gray-900">
-                            Model ID
-                        </h1>
-                        <div className="flex flex-col gap-y-0.5">
-                            <h2 className="text-sm font-medium text-[#494949]">
-                                Define ID Prefix
-                            </h2>
-                            <div className="flex flex-row gap-x-1">
-                                <input
-                                    id="modelIDInput"
-                                    className="bg-gray-200 p-4 rounded-lg placeholder-gray-400 text-[#494949] w-[6.25rem]"
-                                    placeholder="e.g. CNC"
-                                />
-                                <h2 className="text-lg font-medium text-[#494949] mt-3">{`- ${modelId}`}</h2>
-                            </div>
-                        </div>
+                        <ModelIdField modelId={modelId}/>
                         <div className="flex flex-col gap-y-3 mt-8">
                             <AttributeInputColumn attributes={attributes} />
                             <AddAttributeButton setAttributes={setAttributes} />
