599e0b2b1fbe6dc9a6fb927cfa163233df429bd5
tachorzy <tachorzyad@gmail.com>
2024-04-12 16:55:40 -0500

    style: adjusted max-height on attribute, property and name columns

--- code/frontend/components/models/createmodelform/NameField.tsx
+++ code/frontend/components/models/createmodelform/NameField.tsx
@@ -7,14 +7,11 @@ const NameField = (props: { modelId: string }) => {
         <div className="">
             <h1 className="text-2xl font-semibold text-gray-900">Model Name</h1>
             <div className="flex flex-col gap-y-0.5">
-                <h2 className="text-sm font-medium text-[#494949]">
-                    Enter a model name
-                </h2>
                 <div className="flex flex-row gap-x-1">
                     <input
                         id="nameInput"
                         className="bg-gray-200 p-3 rounded-lg placeholder-gray-400 text-[#494949] w-full"
-                        placeholder="e.g. CNC 1"
+                        placeholder="Enter a model name"
                     />
                 </div>
             </div>
