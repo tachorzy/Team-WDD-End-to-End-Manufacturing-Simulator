b1bf4b09a1bb7f40a1ae2b36a25ec797c0184ecc
tachorzy <tachorzyad@gmail.com>
2024-04-12 16:46:31 -0500

    feat: added NameField component in place of ModelIdField

---
+++ code/frontend/app/api/floorplan/route.ts
@@ -0,0 +1,54 @@
+import {
+    BackendConnector,
+    GetConfig,
+    PostConfig,
+} from "@/app/api/_utils/connector";
+import { Floorplan } from "@/app/api/_utils/types";
+
+export async function POST(request: Request) {
+    const payload = (await request.json()) as Floorplan;
+
+    const config: PostConfig<Floorplan> = {
+        resource: "floorplan",
+        payload,
+    };
+
+    try {
+        const data = await BackendConnector.post<Floorplan>(config);
+
+        return new Response(JSON.stringify(data));
+    } catch (error) {
+        console.error(error);
+        return new Response(
+            JSON.stringify({
+                success: false,
+            }),
+        );
+    }
+}
+
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url);
+
+    const floorplanId = searchParams.get("id");
+
+    const config: GetConfig = {
+        resource: "floorplan",
+        params: floorplanId ? { id: floorplanId } : undefined,
+    };
+
+    try {
+        const data = floorplanId
+            ? await BackendConnector.get<Floorplan>(config)
+            : await BackendConnector.get<Floorplan[]>(config);
+
+        return new Response(JSON.stringify(data));
+    } catch (error) {
+        console.error(error);
+        return new Response(
+            JSON.stringify({
+                success: false,
+            }),
+        );
+    }
+}
