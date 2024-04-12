af98eee3a62e26f20cb79fb9bf78253bbd374404
tachorzy <tachorzyad@gmail.com>
2024-04-12 17:36:44 -0500

    feat: added AttributesForm component and useContext

---
+++ code/frontend/components/models/createmodelform/AttributesForm.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useContext } from "react";
+// import Image from "next/image";
+import AttributeInputColumn from "./AttributeInputColumn";
+import AddAttributeButton from "./AddAttributeButton";
+import NameField from "./NameField";
+import { Context } from "./CreateModelForm";
+
+interface AttributesFormContext {
+    factoryId: string;
+    modelId: string;
+    attributes: { attribute: string; value: string }[];
+    setAttributes: React.Dispatch<
+        React.SetStateAction<{ attribute: string; value: string }[]>
+    >;
+    properties: { property: string; unit: string }[];
+    setProperties: React.Dispatch<
+        React.SetStateAction<{ property: string; unit: string }[]>
+    >;
+}
+
+
+const AttributesForm = () => {
+
+    const contextValue = useContext(Context) as AttributesFormContext;
+    
+    return (
+        <div className="flex flex-row gap-x-16 mt-6 gap-y-2">
+            <section className="flex flex-col gap-y-3 min-w-max">
+                <NameField modelId={contextValue?.modelId} />
+            </section>
+
+            <section>
+                <div className="flex flex-col w-full gap-y-3">
+                    <AttributeInputColumn attributes={contextValue?.attributes} />
+                    <AddAttributeButton setAttributes={contextValue?.setAttributes} />
+                </div>
+            </section>
+            <section className="flex flex-row w-[30%] gap-y-3 gap-x-4">
+                <div className="flex flex-col gap-y-2">
+                    <h1 className="text-xl font-semibold text-[#494949]">
+                        Define your attributes
+                    </h1>
+                    <p className="text-base font-medium text-[#494949]">
+                        <span className="text-MainBlue font-semibold">Attributes</span> are inherent qualities of a model that remain constant in the simulation. You can define the attributes of your model and asisgn them their values here.
+                    </p>
+                    <p className="text-base font-medium text-[#494949]">
+                        Common attributes include the model's name, serial number and type.
+                    </p>
+                </div>
+            </section>
+    </div>
+    );
+};
+export default AttributesForm;
--- code/frontend/components/models/createmodelform/CreateModelForm.tsx
+++ code/frontend/components/models/createmodelform/CreateModelForm.tsx
@@ -1,14 +1,11 @@
-import React, { useState } from "react";
-// import Image from "next/image";
-import AttributeInputColumn from "./AttributeInputColumn";
-import PropertyInputColumn from "./PropertyInputColumn";
-import AddAttributeButton from "./AddAttributeButton";
-import AddPropertyButton from "./AddPropertyButton";
-import NameField from "./NameField";
+import React, { useState, useContext } from "react";
+import AttributesForm from "./AttributesForm";
 // interface CreateModelFormProps {
 //     onClose: () => void;
 //     onSave: (formData: Partial<Factory>) => void;
 // }
+
+export const Context = React.createContext({});
 
 const CreateModelForm = (props: { factoryId: string }) => {
     const { factoryId } = props;
@@ -23,31 +20,34 @@ const CreateModelForm = (props: { factoryId: string }) => {
     const [properties, setProperties] = useState([{ property: "", unit: "" }]);
 
     return (
-        <div className="items-center justify-center ml-32">
-            <div className="relative w-11/12 max-h-11/12 bg-white rounded-xl p-8 px-10 border-2 border-[#D7D9DF]">
-                <h1 className="text-3xl font-semibold mb-4 text-gray-900">
-                    Create Your Asset Model
-                </h1>
-                <div className="flex flex-row gap-x-16 mt-6 gap-y-2">
-                    <section className="flex flex-col gap-y-3 min-w-max">
-                        <NameField modelId={modelId} />
-                        <div className="flex flex-col gap-y-3">
-                            <AttributeInputColumn attributes={attributes} />
-                            <AddAttributeButton setAttributes={setAttributes} />
-                        </div>
-                    </section>
+        <Context.Provider value={{ factoryId, modelId, attributes, setAttributes, properties, setProperties }}>
+            <div className="items-center justify-center ml-32">
+                <div className="relative w-11/12 max-h-11/12 bg-white rounded-xl p-8 px-10 border-2 border-[#D7D9DF]">
+                    <h1 className="text-3xl font-semibold mb-4 text-gray-900">
+                        Create Your Asset Model
+                    </h1>
 
-                    <section className="flex flex-row gap-y-3 gap-x-4">
-                        <div className="flex flex-col w-3/4 gap-y-3">
-                            <PropertyInputColumn properties={properties} />
-                            <AddPropertyButton setProperties={setProperties} />
-                        </div>
-                    </section>
+                    <AttributesForm/>
+                    {/* <div className="flex flex-row gap-x-16 mt-6 gap-y-2">
+                        <section className="flex flex-col gap-y-3 min-w-max">
+                            <NameField modelId={modelId} />
+                            <div className="flex flex-col gap-y-3">
+                                <AttributeInputColumn attributes={attributes} />
+                                <AddAttributeButton setAttributes={setAttributes} />
+                            </div>
+                        </section>
 
-                    {/* <ModelIdField modelId={modelId} /> */}
+                        <section className="flex flex-row gap-y-3 gap-x-4">
+                            <div className="flex flex-col w-3/4 gap-y-3">
+                                <PropertyInputColumn properties={properties} />
+                                <AddPropertyButton setProperties={setProperties} />
+                            </div>
+                        </section>
+
+                    </div> */}
                 </div>
             </div>
-        </div>
+        </Context.Provider>
     );
 };
 export default CreateModelForm;
