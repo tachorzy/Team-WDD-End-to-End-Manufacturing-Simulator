9ebe0f31a75c8945404ade9cc50e8df6a7d47375
tachorzy <tachorzyad@gmail.com>
2024-04-13 06:37:15 -0500

    feat: added SineWaveGenerator component

--- code/frontend/components/models/createmodelform/generatordefinition/GeneratorFunctionForm.tsx
+++ code/frontend/components/models/createmodelform/generatordefinition/GeneratorFunctionForm.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import RandomGeneratorForm from "./RandomGeneratorForm";
-
+import SineWaveGeneratorForm from "./SineWaveGeneratorForm";
 const GeneratorFunctionForm = (props: {
     propertyIndex: number }) => {
     const { propertyIndex } = props;
 
     return (
         <div className="flex flex-col gap-y-3 max-h-72">
-            <RandomGeneratorForm ></RandomGeneratorForm>
+            <div className="flex flex-row gap-x-16">
+                <RandomGeneratorForm />
+                <SineWaveGeneratorForm/>
+            </div>
         </div>
     );
 };
--- code/frontend/components/models/createmodelform/generatordefinition/SineWaveGeneratorForm.tsx
+++ code/frontend/components/models/createmodelform/generatordefinition/SineWaveGeneratorForm.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import Image from "next/image";
 
-const RandomGeneratorForm = () => {
+const SineWaveGeneratorForm = () => {
     return (
         <div className="flex flex-col gap-y-3 max-h-72">
             <div>
                 <h1 className="text-2xl font-semibold text-gray-900">Properties</h1>
-                <div className="flex flex-row gap-x-0.5">
-                    <Image src="/icons/generation/random.svg" width={20} height={20} alt="random wave"></Image>
-                    <h2 className="text-sm font-medium text-DarkBlue">Randomized function</h2>
+                <div className="flex flex-row gap-x-1">
+                    <Image src="/icons/generation/sine-waves.svg" width={20} height={20} alt="sine wave"></Image>
+                    <h2 className="text-sm font-medium text-DarkBlue">Sine wave function</h2>
                 </div>
             </div>
                 <div className="flex flex-col gap-y-3">
                     <div className="flex flex-row items-start">
-                        <div className="flex flex-col">
+                        <div className="flex flex-col gap-y-2">
                             <h2 className="text-sm font-medium text-[#494949]">
                                 {"Frequency (ms)"}
                             </h2>
@@ -21,18 +21,32 @@ const RandomGeneratorForm = () => {
                                 className="bg-gray-200 p-3 rounded-lg placeholder-gray-400 text-[#494949] w-11/12"
                                 placeholder="e.g. 60000"
                             />
+                            <h2 className="text-sm font-medium text-[#494949]">
+                                {"Angular frequency (ω)"}
+                            </h2>
+                            <input
+                                className="bg-gray-200 p-3 rounded-lg placeholder-gray-400 text-[#494949] w-11/12"
+                                placeholder="e.g. 1000"
+                            />
                         </div>
                         <div className="flex flex-row">
-                            <div className="flex flex-col w-1/2">
+                            <div className="flex flex-col gap-y-2 w-1/2">
                                 <h2 className="text-sm font-medium text-[#494949]">
-                                    Minimum value
+                                    Amplitude
                                 </h2>
                                 <input
                                     className="bg-gray-200 p-3 rounded-lg placeholder-gray-400 text-[#494949] w-11/12"
-                                    placeholder="e.g. 0"
+                                    placeholder="e.g. 2"
+                                />
+                                <h2 className="text-sm font-medium text-[#494949]">
+                                    Phase
+                                </h2>
+                                <input
+                                    className="bg-gray-200 p-3 rounded-lg placeholder-gray-400 text-[#494949] w-11/12"
+                                    placeholder="e.g. 0.5"
                                 />
                             </div>
-                            <div className="flex flex-col w-1/2">
+                            <div className="flex flex-col gap-y-2 w-1/2">
                                 <h2 className="text-sm font-medium text-[#494949]">
                                     Maximum value
                                 </h2>
@@ -49,4 +63,4 @@ const RandomGeneratorForm = () => {
     );
 };
 
-export default RandomGeneratorForm;
+export default SineWaveGeneratorForm;
