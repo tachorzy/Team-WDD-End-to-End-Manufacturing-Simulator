c03483d39fc201ea4f7c212a9b8f11ed3e5e003e
C. <cairohyfr@gmail.com>
2024-04-13 16:18:41 -0500

    fix mock

--- code/backend/api/internal/mocks/s3.go
+++ code/backend/api/internal/mocks/s3.go
@@ -9,31 +9,28 @@ import (
 
 type S3Client struct {
 	types.S3Client
-	PutObjectFunc    func(ctx context.Context, params *s3.PutObjectInput, optFns ...func(*s3.PutObjectOptions)) (*s3.PutObjectOutput, error)
-	GetObjectFunc    func(ctx context.Context, params *s3.GetObjectInput, optFns ...func(*s3.GetObjectOptions)) (*s3.GetObjectOutput, error)
-	DeleteObjectFunc func(ctx context.Context, params *s3.DeleteObjectInput, optFns ...func(*s3.DeleteObjectOptions)) (*s3.DeleteObjectOutput, error)
+	PutObjectFunc    func(ctx context.Context, params *s3.PutObjectInput, optFns ...func(*s3.Options)) (*s3.PutObjectOutput, error)
+	GetObjectFunc    func(ctx context.Context, params *s3.GetObjectInput, optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error)
+	DeleteObjectFunc func(ctx context.Context, params *s3.DeleteObjectInput, optFns ...func(*s3.Options)) (*s3.DeleteObjectOutput, error)
 }
 
-func (m *S3Client) PutObject(ctx context.Context, params *s3.PutObjectInput, optFns ...func(*s3.PutObjectOptions)) (*s3.PutObjectOutput, error) {
+func (m *S3Client) PutObject(ctx context.Context, params *s3.PutObjectInput, optFns ...func(*s3.Options)) (*s3.PutObjectOutput, error) {
 	if m.PutObjectFunc != nil {
 		return m.PutObjectFunc(ctx, params, optFns...)
 	}
-
 	return &s3.PutObjectOutput{}, nil
 }
 
-func (m *S3Client) GetObject(ctx context.Context, params *s3.GetObjectInput, optFns ...func(*s3.GetObjectOptions)) (*s3.GetObjectOutput, error) {
+func (m *S3Client) GetObject(ctx context.Context, params *s3.GetObjectInput, optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error) {
 	if m.GetObjectFunc != nil {
 		return m.GetObjectFunc(ctx, params, optFns...)
 	}
-
 	return &s3.GetObjectOutput{}, nil
 }
 
-func (m *S3Client) DeleteObject(ctx context.Context, params *s3.DeleteObjectInput, optFns ...func(*s3.DeleteObjectOptions)) (*s3.DeleteObjectOutput, error) {
+func (m *S3Client) DeleteObject(ctx context.Context, params *s3.DeleteObjectInput, optFns ...func(*s3.Options)) (*s3.DeleteObjectOutput, error) {
 	if m.DeleteObjectFunc != nil {
 		return m.DeleteObjectFunc(ctx, params, optFns...)
 	}
-
 	return &s3.DeleteObjectOutput{}, nil
 }
