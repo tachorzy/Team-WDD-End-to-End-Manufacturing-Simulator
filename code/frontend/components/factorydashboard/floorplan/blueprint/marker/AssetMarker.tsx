ddcf4bb90b4b16ea2a23978ab4d5a4b148ffeefc
ReginaldGreen02 <rejogreen@gmail.com>
2024-04-15 12:54:33 -0500

    feat: popup now changes sides depending on closeness to left and right borders

--- code/frontend/components/factorydashboard/floorplan/blueprint/marker/AssetMarker.tsx
+++ code/frontend/components/factorydashboard/floorplan/blueprint/marker/AssetMarker.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import Draggable from "react-draggable";
 import { Asset } from "@/app/api/_utils/types";
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import MarkerPopups from "./MarkerPopups";
+import { DraggableData, DraggableEvent } from "react-draggable";
 
 interface AssetMarkerProps {
     asset: Asset;
@@ -14,6 +15,9 @@ const AssetMarker: React.FC<AssetMarkerProps> = ({ asset }) => {
     const [showEditForm, setShowEditForm] = useState(false);
     const [showButtons, setShowButtons] = useState(false);
     const [isDragging, setIsDragging] = useState(false);
+    const [deltaPosition, setDeltaPosition] = useState({ x: 0, y: 0 });
+    const [isCloseToLeft, setIsCloseToLeft] = useState(true);
+    const [floorplanWidth, setFloorplanWidth] = useState(0);
     const handleDelete = () => {
         setShowConfirmDialog(true);
     };
@@ -36,11 +40,24 @@ const AssetMarker: React.FC<AssetMarkerProps> = ({ asset }) => {
     };
 
  
-const handleDraggableClick = () => {
-    if (!isDragging) {
-        setShowButtons(!showButtons);
+    const handleDraggableClick = () => {
+        if (!isDragging) {
+            setShowButtons(!showButtons);
+        }
+    };
+
+    useEffect(() => {
+        const floorplan = document.querySelector('.floorplan');
+        if (floorplan) {
+            setFloorplanWidth(floorplan.getBoundingClientRect().width);
+        }
+    }, []);
+
+    const handleDrag = (e: DraggableEvent, data: DraggableData) => {
+        setShowButtons(false);
+        setDeltaPosition({ x: data.x, y: data.y });
+        setIsCloseToLeft(data.x < floorplanWidth / 2);
     }
-};
 
 
     return (
@@ -51,7 +68,7 @@ const handleDraggableClick = () => {
             bounds=".floorplan"
             onStart={() => setIsDragging(true)}
             onStop={() => setIsDragging(false)}
-            onDrag={() => setShowButtons(false)}
+            onDrag={handleDrag}
            
               >
                 <div className="group flex flex-col absolute top-0 left-0 z-10 drop-shadow-md items-center">
@@ -68,8 +85,8 @@ const handleDraggableClick = () => {
                         {asset?.name}
                     </p>
                     {showButtons && (
-                        <div className="absolute left-full top-1/2 transform -translate-y-1/2 bg-white shadow-lg rounded-xl p-6 min-w-max w-96 h-auto flex flex-col justify-between no-drag">
-                            <div>
+                        <div className={`absolute ${isCloseToLeft ? 'left-full' : 'right-full'} top-1/2 transform -translate-y-1/2 bg-white shadow-lg rounded-xl p-6 min-w-max w-96 h-auto flex flex-col justify-between no-drag`}>
+                            <div className="">
                                 <h2 className="text-xl font-semibold mb-4 text-gray-900">
                                     Asset Info
                                 </h2>
