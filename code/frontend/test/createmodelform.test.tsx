c407f302af26c801d7e48263d5c7aaa32c132792
tachorzy <tachorzyad@gmail.com>
2024-04-16 01:33:45 -0500

    fix: fixed test cases

--- code/frontend/test/createmodelform.test.tsx
+++ code/frontend/test/createmodelform.test.tsx
@@ -23,6 +23,7 @@ const initialProperty: Property = {
     measurementId: "",
     name: "",
     unit: "",
+    generatorType: "",
 };
 
 describe("CreateModelForm", () => {
@@ -56,6 +57,7 @@ describe("CreateModelForm", () => {
     test("should render PropertyInputColumn input fields", () => {
         const { getByText, getByPlaceholderText } = render(
             <PropertyInputColumn
+                inputFields={[initialProperty]}
                 properties={[initialProperty]}
                 setProperties={jest.fn()}
             />,
