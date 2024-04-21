9d86485eee69cd63100b4e92000d21e4884d1e1b
tachorzy <tachorzyad@gmail.com>
2024-04-21 01:29:20 -0500

    chore: removed left over context in factory dashboard page

--- code/frontend/app/factorydashboard/[factoryId]/page.tsx
+++ code/frontend/app/factorydashboard/[factoryId]/page.tsx
@@ -9,8 +9,6 @@ import FloorManager from "@/components/factorydashboard/floormanager/FloorManage
 import { GetConfig, NextServerConnector } from "@/app/api/_utils/connector";
 import { Floorplan } from "@/app/api/_utils/types";
 
-export const Context = React.createContext({});
-
 export default function FactoryDashboard({
     params,
 }: {
