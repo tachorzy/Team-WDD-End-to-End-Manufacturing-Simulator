13de59becb436e6c17c0328dbf26577375a02fde
tachorzy <tachorzyad@gmail.com>
2024-04-20 19:11:55 -0500

    feat: added asset dashboard page

---
+++ code/frontend/app/factorydashboard/[factoryId]/[assetId]/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+// import { useState } from "react";
+import FactoryPageNavbar from "@/components/Navbar/FactoryPageNavbar";
+import CreateModelForm from "@/components/models/createmodelform/CreateModelForm";
+
+export default function Page({ params }: { params: { factoryId: string, assetId: string } }) {
+    const { factoryId, assetId } = params;
+
+    return (
+        <main className="flex flex-col bg-[#FAFAFA] min-h-screen mx-auto smooth-scroll">
+            <div className="flex flex-col bg-[url('/background/Grid.svg')] min-h-screen rounded-3xl bg-opacity-[15%]">
+                <FactoryPageNavbar
+                    pageId="Dashboard"
+                    factoryId={factoryId}
+                />
+            </div>
+        </main>
+    );
+}
