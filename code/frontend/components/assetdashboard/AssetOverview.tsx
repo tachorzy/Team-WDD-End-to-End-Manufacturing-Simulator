eeb6b90cd36acace22b16380e87ff9a07cb0011e
tachorzy <tachorzyad@gmail.com>
2024-04-22 03:07:27 -0500

    style: restyled text in AssetOverview

--- code/frontend/components/assetdashboard/AssetOverview.tsx
+++ code/frontend/components/assetdashboard/AssetOverview.tsx
@@ -1,23 +1,27 @@
 
-import { Asset } from "@/app/types/types";
+import { Asset } from "@/app/api/_utils/types";
 import React from "react";
 
 const AssetOverview = (props: { asset: Asset }) => {
     const { asset } = props;
 
     console.log(`asset in BentoBox: ${asset}`)
+    
+    const assetName = asset?.name;
+    const assetModel = asset?.modelId;
+    const assetDescription = asset?.description;
 
     return (
         <div
             className="flex flex-col items-center gap-x-2 mb-3 gap-y-2 border-2 border-[#E2E4EA] bg-[#FAFAFA] rounded-lg pt-4 w-80 h-64"
         >
             <h1 className="text-[#494949] text-lg font-medium">Asset Overview</h1>
-            <div className="mt-2 gap-y-1">
-                <h1 className="font-medium">
-                    <span className="text-sm text-[#494949] font-bold">Asset Name:</span> {asset?.name}
+            <div className="mt-2 gap-y-1 px-8">
+                <h1 className="font-medium text-sm text-[#494949] ">
+                    <span className="font-bold">Asset Name:</span> {assetName}
                 </h1>
-                <h1 className="font-medium">
-                    <span className="text-sm text-[#494949] font-bold">Model:</span> {asset?.name}
+                <h1 className="font-medium text-sm text-[#494949] ">
+                    <span className="font-bold">Model:</span> {assetModel}
                 </h1>
             </div>
         </div>
