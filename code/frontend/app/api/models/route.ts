ce9344c35e5d7eb56fffe1f5a82221e3ec876e65
tachorzy <tachorzyad@gmail.com>
2024-04-23 02:03:42 -0500

    feat: added POST request for models

---
+++ code/frontend/app/api/models/route.ts
@@ -0,0 +1,28 @@
+import {
+    BackendConnector,
+    GetConfig,
+    PostConfig,
+} from "@/app/api/_utils/connector";
+import { Model } from "@/app/api/_utils/types";
+
+export async function POST(request: Request) {
+    const payload = (await request.json()) as Model;
+
+    const config: PostConfig<Model> = {
+        resource: "models",
+        payload,
+    };
+
+    try {
+        const data = await BackendConnector.post<Model>(config);
+
+        return new Response(JSON.stringify(data));
+    } catch (error) {
+        console.error(error);
+        return new Response(
+            JSON.stringify({
+                success: false,
+            }),
+        );
+    }
+}
