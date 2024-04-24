53f23027e4e1c56b78bf4d2ff5f7563d6efde651
ReginaldGreen02 <rejogreen@gmail.com>
2024-04-24 18:02:47 -0500

    feat: Login/Sign Up page created

---
+++ code/frontend/app/userauth/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import LoginForm from "@/components/Navbar/LoginForm";
+import SignUpForm from "@/components/Navbar/SignUpForm";
+import LandingPageTitle from "@/components/home/LandingPageTitle";
+
+const AuthPage = () => (
+    <div className="flex flex-col bg-[#FAFAFA] min-h-screen mx-auto smooth-scroll">
+        <div className="flex flex-col bg-[url('/background/Grid.svg')] max-h-1/2 rounded-3xl bg-opacity-[15%]">
+            <LandingPageTitle />
+            <div className="flex flex-row items-center justify-between">
+                <div className="w-1/2 h-full border-r border-gray-300 pr-3">
+                    <LoginForm />
+                </div>
+                <div className="w-1/2 h-full pl-3">
+                    <SignUpForm />
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default AuthPage;
