a94777e56bb8118f9cde9f51e490ddb576c7f328
tachorzy <tachorzyad@gmail.com>
2024-04-26 21:23:31 -0500

    fix: removed reference error

--- code/frontend/components/assetdashboard/charts/ChartColumn.tsx
+++ code/frontend/components/assetdashboard/charts/ChartColumn.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropertyChart from "./PropertyChart";
 
 interface DataPoint {
@@ -7,22 +7,23 @@ interface DataPoint {
 }
 
 const ChartColumn = () => {
-    // testing random data
-    // const measurements = [
+    const [data, setData] = useState<DataPoint[]>([]);
+
+    useEffect(() => {
+    //         const measurements = [
     //     1.85, 8.0, 16.25, 12.5, 0, 10.0, 8.75, 8.54, 8.34, 9.25, 10.75, 11.25,
     //     13.45,
     // ];
-
-    // testing a sine wave.
-    const measurements = Array.from({ length: 100 }, (_, i) => Math.sin(i / 10));
-
-    const data: DataPoint[] = measurements.map((value, index) => ({
-        timeStamp: Date.now() + index * 60000,
-        value,
-    }));
+        const measurements = Array.from({ length: 100 }, (_, i) => i % 20);
+        const newData = measurements.map((value, index) => ({
+            timeStamp: Date.now() + index * 60000,
+            value,
+        }));
+        setData(newData);
+    }, []);
 
     return (
-        <div className="flex flex-col mb-3 gap-y-4 w-full h-64 overflow-y-scroll border-2 border-[#E2E4EA] bg-[#FAFAFA] rounded-lg">
+        <div className="flex flex-col mb-3 pl-5 gap-y-3 w-full h-64 overflow-y-scroll border-2 border-[#E2E4EA] bg-[#FAFAFA] rounded-lg">
             <PropertyChart data={data} />
         </div>
     );
--- code/frontend/components/assetdashboard/charts/PropertyChart.tsx
+++ code/frontend/components/assetdashboard/charts/PropertyChart.tsx
@@ -13,70 +13,68 @@ interface PropertyChartProps {
 }
 
 const LineChart = ({ data }: PropertyChartProps) => {
-    
-    const chartElement = document.getElementById("chart");
-    const width = chartElement ? chartElement.clientWidth : 600;
-    const margin = 50;
-    const height = 240 - 2 * margin;
-
-    const svg = d3
-        .select("#chart")
-        .selectAll("svg")
-        .data([data]) // bind the data to the SVG element
-        .join("svg") // enter + update + exit
-        .attr("width", width + 2 * margin)
-        .attr("height", height + 2 * margin);
-
-    const g = svg
-        .append("g")
-        .attr("transform", `translate(${margin}, ${margin})`);
-
-    const xScale = d3
-        .scaleTime()
-        .range([0, width])
-        .domain(d3.extent(data, (d) => d.timeStamp) as [number, number]);
-
-    const yScale = d3.scaleLinear()
-        .range([height, 0])
-        .domain(d3.extent(data, d => d.value) as [number, number]);
-
-    const xAxis = d3.axisBottom(xScale)
-        .tickFormat((d, i) => d3.timeFormat("%I:%M %p")(d as Date));    
-    
-    const yAxis = d3.axisLeft(yScale);
-
-    const line = d3
-        .line<DataPoint>()
-        .x((d) => xScale(d.timeStamp))
-        .y((d) => yScale(d.value));
-
-    g.append("g")
-        .attr("transform", `translate(0, ${height})`)
-        .call(xAxis)
-        .attr("class", "text-[#494949]")
-        .selectAll("line")
-        .attr("stroke", "#494949")
-        .attr("stroke-width", 0.5);
-
-    g.append("g")
-        .call(yAxis)
-        .attr("class", "text-[#494949]")
-        .selectAll("line")
-        .attr("class", "#494949")
-        .attr("stroke", "#494949")
-        .attr("stroke-width", 0.5);
-
-    g.append("text")
-        .attr("class", "text-[#494949] text-xs")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 0 - margin)
-        .attr("x", 0 - height / 2)
-        .attr("dy", "1em")
-        .style("text-anchor", "middle")
-        .text("Value");    
-
     useEffect(() => {
-
+        const chartElement = document.getElementById("chart");
+        const width = chartElement ? chartElement.clientWidth : 600;
+        const margin = 50;
+        const height = 240 - 2 * margin;
+    
+        const svg = d3
+            .select("#chart")
+            .selectAll("svg")
+            .data([data]) // bind the data to the SVG element
+            .join("svg") // enter + update + exit
+            .attr("width", width + 2 * margin)
+            .attr("height", height + 2 * margin);
+    
+        const g = svg
+            .append("g")
+            .attr("transform", `translate(${margin}, ${margin})`);
+    
+        const xScale = d3
+            .scaleTime()
+            .range([0, width])
+            .domain(d3.extent(data, (d) => d.timeStamp) as [number, number]);
+    
+        const yScale = d3.scaleLinear()
+            .range([height, 0])
+            .domain(d3.extent(data, d => d.value) as [number, number]);
+    
+        const xAxis = d3.axisBottom(xScale)
+            .tickFormat((d, i) => d3.timeFormat("%I:%M %p")(d as Date));    
+        
+        const yAxis = d3.axisLeft(yScale);
+    
+        const line = d3
+            .line<DataPoint>()
+            .x((d) => xScale(d.timeStamp))
+            .y((d) => yScale(d.value));
+    
+        g.append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(xAxis)
+            .attr("class", "text-[#494949]")
+            .selectAll("line")
+            .attr("stroke", "#494949")
+            .attr("stroke-width", 0.5);
+    
+        g.append("g")
+            .call(yAxis)
+            .attr("class", "text-[#494949]")
+            .selectAll("line")
+            .attr("class", "#494949")
+            .attr("stroke", "#494949")
+            .attr("stroke-width", 0.5);
+    
+        g.append("text")
+            .attr("class", "text-[#494949] text-xs")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - margin)
+            .attr("x", 0 - height / 2)
+            .attr("dy", "1em")
+            .style("text-anchor", "middle")
+            .text("Value");    
+    
         g.append("path")
             .datum(data)
             .attr("d", line)
@@ -85,7 +83,7 @@ const LineChart = ({ data }: PropertyChartProps) => {
             .attr("fill", "none");
     }, [data]);
 
-    return <div id="chart" className="w-11/12 my-5" />;
+    return <div id="chart" className="w-11/12 my-3" />;
 };
 
 export default LineChart;
