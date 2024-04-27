ffe10a5b449ad31d1348291afd52600ed2f67f29
tachorzy <tachorzyad@gmail.com>
2024-04-26 23:34:39 -0500

    fix: lint fix

--- code/frontend/components/assetdashboard/charts/ChartColumn.tsx
+++ code/frontend/components/assetdashboard/charts/ChartColumn.tsx
@@ -10,7 +10,8 @@ const ChartColumn = () => {
     const [data, setData] = useState<DataPoint[]>([]);
     // there is mock data for now
     const [measurements, setMeasurements] = useState([
-        1.85, 8.0, 16.25, 12.5, 0, 10.0, 8.75, 8.54, 8.34, 9.25, 10.75, 11.25, 13.45,
+        1.85, 8.0, 16.25, 12.5, 0, 10.0, 8.75, 8.54, 8.34, 9.25, 10.75, 11.25,
+        13.45,
     ]);
 
     useEffect(() => {
@@ -24,11 +25,11 @@ const ChartColumn = () => {
         const intervalId = setInterval(() => {
             setMeasurements((prev) => {
                 const lastValue = prev[prev.length - 1];
-                const nextValue = Math.random() * 22 - 2;
-                return [...prev.slice(1), lastValue + nextValue];
+                const nextValue = Math.random() * 22 - 0;
+                return [...prev.slice(1), lastValue + nextValue * -1];
             });
         }, 6000);
-    
+
         return () => {
             clearInterval(intervalId); // clear the interval when the component unmounts
         };
--- code/frontend/components/assetdashboard/charts/PropertyChart.tsx
+++ code/frontend/components/assetdashboard/charts/PropertyChart.tsx
@@ -20,7 +20,7 @@ const LineChart = ({ data }: PropertyChartProps) => {
         const width = chartElement ? chartElement.clientWidth : 600;
         const margin = 50;
         const height = 240 - 2 * margin;
-    
+
         const svg = d3
             .select("#chart")
             .selectAll("svg")
@@ -28,30 +28,32 @@ const LineChart = ({ data }: PropertyChartProps) => {
             .join("svg") // enter + update + exit
             .attr("width", width + 2 * margin)
             .attr("height", height + 2 * margin);
-    
+
         const g = svg
             .append("g")
             .attr("transform", `translate(${margin}, ${margin})`);
-    
+
         const xScale = d3
             .scaleTime()
             .range([0, width])
             .domain(d3.extent(data, (d) => d.timeStamp) as [number, number]);
-    
-        const yScale = d3.scaleLinear()
+
+        const yScale = d3
+            .scaleLinear()
             .range([height, 0])
-            .domain(d3.extent(data, d => d.value) as [number, number]);
-    
-        const xAxis = d3.axisBottom(xScale)
-            .tickFormat((d, i) => d3.timeFormat("%I:%M %p")(d as Date));    
-        
+            .domain(d3.extent(data, (d) => d.value) as [number, number]);
+
+        const xAxis = d3
+            .axisBottom(xScale)
+            .tickFormat((d, i) => d3.timeFormat("%I:%M %p")(d as Date));
+
         const yAxis = d3.axisLeft(yScale);
-    
+
         const line = d3
             .line<DataPoint>()
             .x((d) => xScale(d.timeStamp))
             .y((d) => yScale(d.value));
-    
+
         g.append("g")
             .attr("transform", `translate(0, ${height})`)
             .call(xAxis)
@@ -59,7 +61,7 @@ const LineChart = ({ data }: PropertyChartProps) => {
             .selectAll("line")
             .attr("stroke", "#494949")
             .attr("stroke-width", 0.5);
-    
+
         g.append("g")
             .call(yAxis)
             .attr("class", "text-[#494949]")
@@ -67,7 +69,7 @@ const LineChart = ({ data }: PropertyChartProps) => {
             .attr("class", "#494949")
             .attr("stroke", "#494949")
             .attr("stroke-width", 0.5);
-    
+
         g.append("text")
             .attr("class", "text-[#494949] text-xs")
             .attr("transform", "rotate(-90)")
@@ -75,8 +77,8 @@ const LineChart = ({ data }: PropertyChartProps) => {
             .attr("x", 0 - height / 2)
             .attr("dy", "1em")
             .style("text-anchor", "middle")
-            .text("Value");    
-    
+            .text("Value");
+
         g.append("path")
             .datum(data)
             .attr("d", line)
@@ -84,27 +86,26 @@ const LineChart = ({ data }: PropertyChartProps) => {
             .attr("stroke-width", 1.5)
             .attr("fill", "none");
 
+        const svgEnter = svg.enter().append("svg");
+        svgEnter.append("path").attr("class", "line");
+        svgEnter.append("g").attr("class", "x-axis");
+        svgEnter.append("g").attr("class", "y-axis");
 
-    const svgEnter = svg.enter().append("svg");
-    svgEnter.append("path").attr("class", "line");
-    svgEnter.append("g").attr("class", "x-axis");
-    svgEnter.append("g").attr("class", "y-axis");    
+        // handle the update selection
+        const svgUpdate = svg
+            .merge(svgEnter)
+            .attr("width", width + 2 * margin)
+            .attr("height", height + 2 * margin);
 
-// handle the update selection
-    const svgUpdate = svg.merge(svgEnter)
-    .attr("width", width + 2 * margin)
-    .attr("height", height + 2 * margin);
+        svgUpdate
+            .select(".x-axis")
+            .attr("transform", `translate(0, ${height})`)
+            .call(d3.axisBottom(xScale));
 
+        svgUpdate.select(".y-axis").call(d3.axisLeft(yScale));
 
-    svgUpdate.select(".x-axis")
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(xScale));
-
-    svgUpdate.select(".y-axis")
-    .call(d3.axisLeft(yScale));
-
-    // handle the exit selection
-    svg.exit().remove();
+        // handle the exit selection
+        svg.exit().remove();
     }, [data]);
 
     return <div id="chart" className="w-11/12 my-3" />;
