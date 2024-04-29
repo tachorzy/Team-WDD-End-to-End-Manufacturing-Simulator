b4662a48891cb937a3841d106788e750c78b9ca4
C. <cairohyfr@gmail.com>
2024-04-28 21:54:25 -0500

    hate this

--- code/backend/api/internal/handlers/measurements/create_test.go
+++ code/backend/api/internal/handlers/measurements/create_test.go
@@ -94,24 +94,19 @@ func TestHandleCreateMeasurementRequest_JSONMarshalError(t *testing.T) {
 			return &dynamodb.PutItemOutput{}, nil
 		},
 	}
-
 	handler := NewCreateMeasurementHandler(mockDDBClient)
-
-	request := events.APIGatewayProxyRequest{
-		Body: `{"frequency":"cairo","lowerBound":0.0,"upperBound":10.0,"precision":0.1}`,
-	}
-
 	originalJSONMarshal := wrappers.JSONMarshal
-
 	defer func() { wrappers.JSONMarshal = originalJSONMarshal }()
-
 	wrappers.JSONMarshal = func(v interface{}) ([]byte, error) {
 		return nil, errors.New("mock marshal error")
 	}
 
+	request := events.APIGatewayProxyRequest{
+		Body: `{"frequency":1.0,"generatorFunction":"Function 1","lowerBound":0.0,"upperBound":10.0,"precision":0.1}`,
+	}
+
 	ctx := context.Background()
 	response, err := handler.HandleCreateMeasurementRequest(ctx, request)
-
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
