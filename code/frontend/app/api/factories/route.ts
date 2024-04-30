d251fb2a3d6bb179658955a6519491f89e6d6f5d
tachorzy <tachorzyad@gmail.com>
2024-04-30 16:17:48 -0500

    feat: added property backend connectors

--- code/frontend/app/api/factories/route.ts
+++ code/frontend/app/api/factories/route.ts
@@ -2,20 +2,19 @@ import {
     BackendConnector,
     GetConfig,
     PostConfig,
-    PutConfig,
 } from "@/app/api/_utils/connector";
-import { Factory } from "@/app/api/_utils/types";
+import { Property } from "@/app/api/_utils/types";
 
 export async function POST(request: Request) {
-    const payload = (await request.json()) as Factory;
+    const payload = (await request.json()) as Property;
 
-    const config: PostConfig<Factory> = {
+    const config: PostConfig<Property> = {
         resource: "factories",
         payload,
     };
 
     try {
-        const data = await BackendConnector.post<Factory>(config);
+        const data = await BackendConnector.post<Property>(config);
 
         return new Response(JSON.stringify(data));
     } catch (error) {
@@ -31,44 +30,18 @@ export async function POST(request: Request) {
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
 
-    const factoryId = searchParams.get("id");
+    const propertyId = searchParams.get("id");
     const config: GetConfig = {
         resource: "factories",
-        params: factoryId ? { id: factoryId } : undefined,
+        params: propertyId ? { id: propertyId } : undefined,
     };
 
     try {
-        const data = factoryId
-            ? await BackendConnector.get<Factory>(config)
-            : await BackendConnector.get<Factory[]>(config);
+        const data = propertyId
+            ? await BackendConnector.get<Property>(config)
+            : await BackendConnector.get<Property[]>(config);
 
         return new Response(JSON.stringify(data));
-    } catch (error) {
-        console.error(error);
-        return new Response(
-            JSON.stringify({
-                success: false,
-            }),
-        );
-    }
-}
-
-export async function PUT(request: Request) {
-    const payload = (await request.json()) as Factory;
-
-    const config: PutConfig<Factory> = {
-        resource: "factories",
-        payload,
-    };
-
-    try {
-        await BackendConnector.put<Factory>(config);
-
-        return new Response(
-            JSON.stringify({
-                success: true,
-            }),
-        );
     } catch (error) {
         console.error(error);
         return new Response(
