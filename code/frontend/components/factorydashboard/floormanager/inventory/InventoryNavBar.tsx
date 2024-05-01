c02b82cf238207f3aa2d624615769af63f235f2a
tachorzy <tachorzyad@gmail.com>
2024-05-01 00:05:20 -0500

    refactor: fixed state of activeNavItem in the InventoryNavBar

--- code/frontend/components/factorydashboard/floormanager/inventory/InventoryNavBar.tsx
+++ code/frontend/components/factorydashboard/floormanager/inventory/InventoryNavBar.tsx
@@ -42,15 +42,15 @@ const InventoryNavBar = (props: { factoryId: string }) => {
                 <button
                     key={id}
                     type="button"
-                    onClick={() => setActiveNavItem(model.attributes[0].name)}
+                    onClick={() => setActiveNavItem(model.modelId)}
                     className={`${
-                        model.attributes[0].name === activeNavItem
+                        model.modelId === activeNavItem
                             ? "text-MainBlue"
                             : "text-[#494949]"
                     }  group cursor-pointer font-medium text-xs mt-1.5`}
                 >
                     {model.attributes[0].value}
-                    {model.attributes[0].name === activeNavItem ? (
+                    {model.modelId === activeNavItem ? (
                         <span className="block max-w-0 max-w-full transition-all duration-500 pt-[0.2rem] bg-MainBlue -mb-1.5 z-50 relative" />
                     ) : (
                         <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 pt-[0.2rem] bg-MainBlue -mb-1.5 z-50 relative" />
