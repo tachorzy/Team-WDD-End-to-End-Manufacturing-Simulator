6af03242a204a5cd770b8013f982c7b35f3cf1d9
tachorzy <tachorzyad@gmail.com>
2024-05-01 00:01:53 -0500

    feat: added user defined model headers to inventoryNavBar

--- code/frontend/components/factorydashboard/floormanager/inventory/InventoryNavBar.tsx
+++ code/frontend/components/factorydashboard/floormanager/inventory/InventoryNavBar.tsx
@@ -38,19 +38,19 @@ const InventoryNavBar = (props: { factoryId: string }) => {
 
     return (
         <div className="flex self-start flex-row gap-x-3">
-            {navbarLinks.map((navItem, id) => (
+            {models.map((model, id) => (
                 <button
                     key={id}
                     type="button"
-                    onClick={() => setActiveNavItem(navItem.label)}
+                    onClick={() => setActiveNavItem(model.attributes[0].name)}
                     className={`${
-                        navItem.label === activeNavItem
+                        model.attributes[0].name === activeNavItem
                             ? "text-MainBlue"
                             : "text-[#494949]"
                     }  group cursor-pointer font-medium text-xs mt-1.5`}
                 >
-                    {navItem.label}
-                    {navItem.label === activeNavItem ? (
+                    {model.attributes[0].value}
+                    {model.attributes[0].name === activeNavItem ? (
                         <span className="block max-w-0 max-w-full transition-all duration-500 pt-[0.2rem] bg-MainBlue -mb-1.5 z-50 relative" />
                     ) : (
                         <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 pt-[0.2rem] bg-MainBlue -mb-1.5 z-50 relative" />
