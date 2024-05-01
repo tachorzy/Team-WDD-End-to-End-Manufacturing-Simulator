eeafe4e9ebe4c3e946ca42f50c7ae706a02fb0a2
ReginaldGreen02 <rejogreen@gmail.com>
2024-05-01 13:22:18 -0500

    fix: linted  AssetInventory

--- code/frontend/components/factorydashboard/floormanager/inventory/AssetInventory.tsx
+++ code/frontend/components/factorydashboard/floormanager/inventory/AssetInventory.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { Asset, Model } from "@/app/api/_utils/types";
-import { BackendConnector, GetConfig } from "@/app/api/_utils/connector";
+import React from "react";
+import { Asset } from "@/app/api/_utils/types";
+
 import AssetItem from "./AssetItem";
 
 interface AssetInventoryProps {
@@ -15,27 +15,24 @@ const AssetInventory: React.FC<AssetInventoryProps> = ({
     setSelectedAsset,
     selectedAsset,
     activeNavItem,
-}) => { 
-
-    return (
-        <div className="asset-inventory relative z-10 w-full h-[55%] overflow-y-scroll text-white text-lg flex flex-row flex-wrap border-2 border-[#D7D9DF] border-solid">
-            {assets.length > 0 ? (
-                assets
-                    .filter((asset) => asset.modelId === activeNavItem)
-                    .map((asset) => (
-                        <AssetItem
-                            key={asset.assetId}
-                            asset={asset}
-                            setSelectedAsset={setSelectedAsset}
-                            selectedAsset={selectedAsset}
-                        />
-                    ))
-            ) : (
-                <p className="col-span-3 text-black opacity-[25%] p-2 pl-2.5">
-                    No assets found.
-                </p>
-            )}
-        </div>
-    );
-}
+}) => (
+    <div className="asset-inventory relative z-10 w-full h-[55%] overflow-y-scroll text-white text-lg flex flex-row flex-wrap border-2 border-[#D7D9DF] border-solid">
+        {assets.length > 0 ? (
+            assets
+                .filter((asset) => asset.modelId === activeNavItem)
+                .map((asset) => (
+                    <AssetItem
+                        key={asset.assetId}
+                        asset={asset}
+                        setSelectedAsset={setSelectedAsset}
+                        selectedAsset={selectedAsset}
+                    />
+                ))
+        ) : (
+            <p className="col-span-3 text-black opacity-[25%] p-2 pl-2.5">
+                No assets found.
+            </p>
+        )}
+    </div>
+);
 export default AssetInventory;
