63ab7197e085c2b118837c60af8f9c2bab904a24
tachorzy <tachorzyad@gmail.com>
2024-05-01 02:38:54 -0500

    feat: readded filtering asset inventory by modelId

--- code/frontend/components/factorydashboard/floormanager/inventory/AssetInventory.tsx
+++ code/frontend/components/factorydashboard/floormanager/inventory/AssetInventory.tsx
@@ -6,23 +6,27 @@ interface AssetInventoryProps {
     assets?: Asset[];
     setSelectedAsset: React.Dispatch<React.SetStateAction<Asset | null>>;
     selectedAsset: Asset | null;
+    activeNavItem: string;
 }
 
 const AssetInventory: React.FC<AssetInventoryProps> = ({
     assets = [],
     setSelectedAsset,
     selectedAsset,
+    activeNavItem,
 }) => (
     <div className="asset-inventory relative z-10 w-full h-[55%] overflow-y-scroll text-white text-lg flex flex-row flex-wrap border-2 border-[#D7D9DF] border-solid">
         {assets.length > 0 ? (
-            assets.map((asset) => (
-                <AssetItem
-                    key={asset.assetId}
-                    asset={asset}
-                    setSelectedAsset={setSelectedAsset}
-                    selectedAsset={selectedAsset}
-                />
-            ))
+            assets
+                .filter((asset) => asset.modelId === activeNavItem)
+                .map((asset) => (
+                    <AssetItem
+                        key={asset.assetId}
+                        asset={asset}
+                        setSelectedAsset={setSelectedAsset}
+                        selectedAsset={selectedAsset}
+                    />
+                ))
         ) : (
             <p className="col-span-3 text-black opacity-[25%] p-2 pl-2.5">
                 No assets found.
